fde79f7c82b8c73b8d6716433b79ff0b39a07572
carsten-forty2 <carsten@gimli.org>
2011-08-23 16:18:37 +0000

    minor + transfer

--- pygimli/buildScripts/buildBoostWin32.sh
+++ pygimli/buildScripts/buildBoostWin32.sh
@@ -1,23 +1,31 @@
-BOOST_VERSION=1_43	_0
-BOOST_SRC=/e
-BOOST_SRC=/c/Home
+BOOST_VERSION=1_47_0
+
+if [ $# -eq 0 ]; then
+	prefix=`pwd`
+else 
+	prefix=`readlink -m $1`
+fi
+
+echo "Installing boost at " $prefix
+
+BOOST_SRC=$prefix
+#BOOST_SRC=/c/Home
 
 if ( (python --version) );then
 	PYTHON_ROOT=`python -c "import sys; print sys.prefix.replace(\"\\\\\\\\\",\"/\")"`
 	echo "... found at $PYTHON_ROOT, good"
 else
-	echo "need python2.6 installation"
+	echo "need > python2.6 installation"
 	echo "get one from http://www.python.org/"
-	echo "if allready ensure python26 installation directory is in your PATH"
+	echo "if allready done, ensure python26 installation directory is in your PATH"
 	exit
 fi
 
 BOOST_SRC_DIR=$BOOST_SRC/boost_$BOOST_VERSION
 GCCVER=mingw-`gcc -v 2>&1 | tail -n1 | cut -d' ' -f3`
 
-OLDDIR=$PWD
+pushd $BOOST_SRC_DIR
 
-cd $BOOST_SRC_DIR
 DISTDIR=$BOOST_SRC_DIR/boost_$BOOST_VERSION-$GCCVER
 
 echo calling from $OLDDIR
@@ -31,10 +39,12 @@ fi
 # edit ./tools/v2/build/tools/python.jam:486
 # edit ./tools/build/v2/tools/python.jam:486
 # and remove quotes to python-cmd = $(python-cmd) ;
+# i.e., python-cmd = \"$(python-cmd)\" ;    -->  python-cmd = $(python-cmd) ;
 
 ./bootstrap.sh --prefix=$DISTDIR --with-bjam=./bjam.exe --with-toolset=gcc \
 		--with-python-root=$PYTHON_ROOT --with-libraries=python,system,thread,regex
 	
-./bjam --prefix=$DISTDIR --layout=tagged --build-type=complete --variant=release install
+./b2 --prefix=$DISTDIR --layout=tagged --build-type=complete --variant=release install
 
-cd $OLDDIR
+cp -r $DISTDIR ../boost
+popd
