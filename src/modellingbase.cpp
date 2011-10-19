40cd76c9dda3c5b9b9b0e9d0bac76f12c7d8f8c3
carsten-forty2 <carsten@gimli.org>
2011-10-19 13:57:56 +0000

    add regionManager::addRegion

--- src/modellingbase.cpp
+++ src/modellingbase.cpp
@@ -183,7 +183,6 @@ void ModellingBase::mapModel( const RVector & model, double background ){
     std::vector< Cell * > emptyList;
     mesh_->createNeighbourInfos();
 
-
     for ( uint i = 0, imax = mesh_->cellCount(); i < imax; i ++ ){
         marker = mesh_->cell( i ).marker();
         if ( marker >= 0 ) {
