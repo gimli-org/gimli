a3f25be22a5d31780f0a40e10cadcf69e0759efb
halbmy <halbmy@gmail.com>
2012-01-16 09:44:01 +0000

    added ttinvOffset to ttdijkstramodelling.h, added jacobian calculation time to inversion.h

--- src/ttdijkstramodelling.h
+++ src/ttdijkstramodelling.h
@@ -24,6 +24,11 @@
 
 #include "gimli.h"
 #include "modellingbase.h"
+#include "regionmanager.h"
+#include "dataContainer.h"
+#include "blockmatrix.h"
+#include "meshgenerators.h"
+#define NEWREGION 33333
 
 namespace GIMLI {
     class Mesh;
@@ -117,33 +122,62 @@ protected:
     double background_;
 };
 
-// so ähnlich soll die neue Physik werden: Traveltime mit Offset
-//     namespace GIMLI {
-//     class TravelTimeDijkstraModellingOffset : public TravelTimeDijkstraModelling {
-//       public:
-//         TravelTimeDijkstraModellingOffset( Mesh & mesh, DataContainer & data )
-//         : TravelTimeDijkstraModelling( mesh, data ) {
-//           std::vector < size_t > sources( mesh_->findNodesIdxByMarker( MARKER_NODE_ELECTRODE ) );
-//           nShots_ = sources.size();
-//           offsets_ = RVector( nShots_ );
-//         } // too much
-//
-//         virtual ~TravelTimeDijkstraModellingOffset(){ }
-//
-//         virtual void createJacobian( DSparseMapMatrix & jacobian, const RVector & slowness ){
-//           TravelTimeDijkstraModelling::createJacobian( jacobian, slowness ); // normal J
-//           jacobian.setCols( jacobian.cols() + nShots_ ); // resize jacobian / add col for each shot
-//           for ( size_t dataIdx = 0; dataIdx < dataContainer_->size(); dataIdx ++ ){
-//             jacobian[ dataIdx ][ (*dataContainer_)( dataIdx ).aIdx() ] = 1.0; // additional "way"
-//           }
-//         }
-//
-//         RVector response( const RVector & slowness, double background = 1e16 );
-//
-//     protected:
-//         size_t nShots_;
-//         RVector offsets_;
-//     };
+/*! New Class derived from standard travel time modelling */
+class TTModellingWithOffset: public TravelTimeDijkstraModelling{
+public:
+    TTModellingWithOffset( Mesh & mesh, DataContainer & dataContainer, bool verbose) :
+        TravelTimeDijkstraModelling( mesh, dataContainer, verbose ) {
+        //! find occuring shots, and map them to indices starting from zero
+        shots_ = unique( sort( dataContainer.get("s") ) );
+        std::cout << "found " << shots_.size() << " shots." << std::endl;
+        for ( size_t i = 0 ; i < shots_.size() ; i++ ) {
+            shotMap_.insert( std::pair< int, int >( shots_[ i ], i ) );
+        }
+        //! create new region containing offsets with special marker
+        offsetMesh_ = createMesh1D( shots_.size() );
+        for ( size_t i = 0 ; i < offsetMesh_.cellCount() ; i++ ) offsetMesh_.cell( i ).setMarker( NEWREGION );
+        regionManager().addRegion( NEWREGION, offsetMesh_ );
+    }
+
+    virtual ~TTModellingWithOffset() { }
+
+    RVector createDefaultStartModel() { 
+        return cat( TravelTimeDijkstraModelling::createDefaultStartModel(), RVector( shots_.size() ) );
+    }
+    
+    RVector response( const RVector & model ) {
+        //! extract slowness from model and call old function
+        RVector slowness( model, 0, model.size() - shots_.size() );
+        RVector offsets( model, model.size() - shots_.size(), model.size() ); 
+        RVector resp = TravelTimeDijkstraModelling::response( slowness ); //! normal response
+        RVector shotpos = dataContainer_->get( "s" );
+        for ( size_t i = 0; i < resp.size() ; i++ ){
+            resp[ i ] += offsets[ shotMap_[ shotpos[ i ] ] ];
+        }
+        return resp;
+    }
+
+    void createJacobian( H2SparseMapMatrix1 & jacobian, const RVector & model ) { 
+        //! extract slowness from model and call old function
+        RVector slowness( model, 0, model.size() - shots_.size() );
+        RVector offsets( model, model.size() - shots_.size(), model.size() );         
+        TravelTimeDijkstraModelling::createJacobian( *jacobian.H1(), slowness );
+        jacobian.H2()->setRows( dataContainer_->size() );
+        jacobian.H2()->setCols( offsets.size() );
+        //! set 1 entries for the used shot
+        RVector shotpos = dataContainer_->get( "s" ); // shot=C1/A
+        for ( size_t i = 0; i < dataContainer_->size(); i++ ) {
+            jacobian.H2()->setVal( i, shotMap_[ shotpos[ i ] ], 1.0 ); 
+        }
+    }
+    
+    size_t nShots(){ return shots_.size(); }
+protected:
+    RVector shots_;
+    std::map< int, int > shotMap_;
+    Mesh offsetMesh_;
+};
+
 
 } //namespace GIMLI
 
