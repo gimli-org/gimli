28dbb180edbc5c8016154d2f01a870222d069b98
halbmy <halbmy@gmail.com>
2012-06-26 10:41:49 +0000

    some ideas on block matrices

--- src/blockmatrix.h
+++ src/blockmatrix.h
@@ -29,8 +29,8 @@
 //! block matrices for easier inversion, see appendix E in GIMLi tutorial
 namespace GIMLI{
     
-/*! simple example for tutorial purposes */
-/*! Sparse Block Matrix consisting of two horizontally pasted matrices */
+/*! Simple example for tutorial purposes. */
+/*! Block Matrix consisting of two horizontally pasted sparse map matrices. */
 class DLLEXPORT H2SparseMapMatrix : public MatrixBase{
 public:
     
@@ -44,19 +44,19 @@ public:
     
     virtual void clear() { H1_.clear(); H2_.clear(); }
     
-    /*! Return this * a  */
+    /*! Return this * a . */
     virtual RVector mult( const RVector & a ) const {
         return H1_ * a( 0, H1_.cols() ) + H2_ * a( H1_.cols(), cols() );
     }
     
-    /*! Return this.T * a */
+    /*! Return this.T * a = ( a.T * this ).T . */
     virtual RVector transMult( const RVector & a ) const {
         return cat( H1_.transMult( a ), H2_.transMult( a ) );
     }
     
+    /*! Return references to the 2 matriced (const and non-const, why?). */
     inline const DSparseMapMatrix & H1() const { return H1_; }
     inline const DSparseMapMatrix & H2() const { return H2_; }
-
     inline DSparseMapMatrix & H1() { return H1_; }
     inline DSparseMapMatrix & H2() { return H2_; }
 
@@ -74,7 +74,8 @@ inline bool save( const H2SparseMapMatrix & A, const std::string & filename, IOF
     CERR_TO_IMPL
     return false;
 }
-  
+
+/*! Do we have to do this for every matrix type?? */  
 inline RVector operator * ( const H2SparseMapMatrix & A, const RVector & x ){
     return A.H1() * x( 0, A.H1().cols() ) + A.H2() * x( A.H1().cols(), A.cols() );
 }
@@ -83,17 +84,98 @@ inline RVector transMult( const H2SparseMapMatrix & A, const RVector & b ){
     return cat( transMult( A.H1(), b ), transMult( A.H2(), b ) );
 }
 
-template< class Matrix1, class Matrix2 > class H2Matrix{ };
-template< class Matrix1, class Matrix2 > class V2Matrix{ };
-template< class Matrix1, class Matrix2 > class D2Matrix{ };
+/*! Block matrix with 2 arbitrary matrices pasted horizontally. */
+template< class Matrix1, class Matrix2 > class H2Matrix{ 
+public:
+    H2Matrix(){}
+    
+    virtual ~H2Matrix(){}
 
-template< class Matrix > class HNMatrix{ };
-template< class Matrix > class VNMatrix{ };
-template< class Matrix > class DNMatrix{ };
+    /*! Return rows and columns. */
+    virtual Index rows() const { return H1_.rows(); }
+    virtual Index cols() const { return H1_.cols() + H2_.cols(); }
 
-template< class Matrix > class HRMatrix{ };
-template< class Matrix > class VRMatrix{ };
-template< class Matrix > class DRMatrix{ };
+    /*! Return this * a . */
+    virtual RVector mult( const RVector & a ) const {
+        return H1_ * a( 0, H1_.cols() ) + H2_ * a( H1_.cols(), cols() );
+    }
+    
+    /*! Return this.T * a = ( a.T * this ).T . */
+    virtual RVector transMult( const RVector & a ) const {
+        return cat( H1_.transMult( a ), H2_.transMult( a ) );
+    }
+protected:
+    Matrix1 H1_;
+    Matrix2 H2_;
+
+}; // H2Matrix
+
+/*! Block matrix with 2 arbitrary matrices pasted vertically. */
+template< class Matrix1, class Matrix2 > class V2Matrix : public MatrixBase{ };
+/*! Block diagonal matrix with 2 arbitrary matrices as diagonals. */
+template< class Matrix1, class Matrix2 > class D2Matrix : public MatrixBase{ };
+
+/*! Block matrix with arbitrary number of matrices of the same type pasted horizontally. */
+template< class Matrix > class HNMatrix : public MatrixBase{ 
+public:
+    HNMatrix(){}
+    
+    virtual ~HNMatrix(){}
+    
+    void push_back( Matrix Mat ){ //** standard procedure to build up matrix
+        if ( Mats_.size() > 0 && Mats_[0].nrows() == Mat.rows() ) {
+            Mats_.push_back( Mat ); 
+        } else {
+            throwLengthError( 1, WHERE_AM_I + " matrix rows do not match " +
+                                 toStr( Mats_[0].nrows() ) + " " + toStr( Mat.nrows() ) );
+        }
+    }
+    /*! Return rows and columns. */
+    virtual Index rows() const { 
+        if ( Mats_.size() > 0 ) return Mats_[0].rows(); 
+        return 0;
+    }
+    virtual Index cols() const { 
+        Index ncols = 0;
+        for ( Index i=0 ; i < Mats_.cols() ; i++ ) ncols += Mats_[i].cols();
+        return ncols; 
+    }
+
+protected:
+    std::vector < Matrix > Mats_;
+}; // HNMatrix
+
+/*! Block matrix with arbitrary number of matrices of the same type pasted vertically. */
+template< class Matrix > class VNMatrix : public MatrixBase{ };
+/*! Block diagonal matrix with arbitrary number of matrices of the same type. */
+template< class Matrix > class DNMatrix : public MatrixBase{ };
+
+/*! Block matrix with one matrix repeatedly pasted horizontally. */
+template< class Matrix > class HRMatrix : public MatrixBase{ 
+public:
+    /*! Constructors */
+    HRMatrix(){} //useful?
+    HRMatrix( Matrix Mat) : Mat_( Mat ){}
+    HRMatrix( Matrix Mat, Index nmats ) : Mat_( Mat ), nmats_( nmats ){}
+    
+    ~HRMatrix(){}
+    
+    void setNumber( Index nmats ){ nmats_ = nmats; }
+    /*! Return rows and columns. */
+    virtual Index rows() const { return Mat_.rows(); }
+    virtual Index cols() const { return Mat_.cols() * nmats_; }
+
+    inline const Matrix & Mat() const { return Mat_; }
+    inline Matrix & Mat() { return Mat_; }
+protected:
+    Matrix Mat_;
+    Index nmats_;
+}; // HRMatrix
+
+/*! Block matrix with one matrix repeatedly pasted vertically. */
+template< class Matrix > class VRMatrix : public MatrixBase{ };
+/*! Block diagonal matrix with one matrix repeatedly as diagonals. */
+template< class Matrix > class DRMatrix : public MatrixBase{ };
 
 //** specializations better moved into gimli.h
 //! nomenclature: Type(R/S)+Alignment(H/V/D)+Number(2/N/R)+Matrix
@@ -101,6 +183,12 @@ typedef H2Matrix< RMatrix, RMatrix > RH2Matrix;
 typedef H2Matrix< SparseMapMatrix< double, Index >, SparseMapMatrix< double, Index > > SH2Matrix;
 typedef DRMatrix< RMatrix > RDRMatrix;
 typedef DRMatrix< SparseMapMatrix< double, Index > > SDRMatrix;
+
+//! Some examples useful for special inversions
+typedef H2Matrix< IdentityMatrix, IdentityMatrix > TwoModelsCMatrix; // -I +I
+typedef DRMatrix< TwoModelsCMatrix > ManyModelsCMatrix;  //** not really diagonal!
+typedef DRMatrix< DSparseMapMatrix > ManyCMatrix;
+typedef V2Matrix< ManyCMatrix, ManyModelsCMatrix > MMMatrix; // Multiple models (LCI,timelapse)
 
 } // namespace GIMLI
 
