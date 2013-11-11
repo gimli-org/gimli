99417cb654a0d02ee1e377a304a2fecd1f5d28ab
halbmy <halbmy@gmail.com>
2013-11-11 18:50:32 +0000

    added some EM python stuff and moved into physics/em

---
+++ python/pygimli/physics/em/fdem.py
@@ -0,0 +1,420 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+    Was macht das ding
+
+'''
+
+import pygimli as g
+
+import pylab as P
+from pygimli import FDEM1dModelling, RVector, asvector, RTrans, RTransLog, RTransLogLU, RInversion
+
+def importEmsysAsciiData( filename, verbose = False ):
+    """ pure import function reading in positions, data, frequencies, error and geometry """
+    
+    xx, sep, f, pf, ip, op, hmod, q = P.loadtxt( filename, skiprows=1, usecols=(1,4,6,8,9,12,15,16 ), unpack=True )
+
+    err = q / pf  * 100. # percentage of primary field
+    
+    if len( P.unique( sep ) ) > 1:
+        print "Warning! Several coil spacings present in file!"
+    
+    coilspacing = P.median( sep )
+    
+    f = P.round_( f )
+    freq, mf, nf = P.unique( f, True, True )
+    x, mx, nx = P.unique( xx, True, True )
+    IP = P.ones( ( len(x), len(freq) ) ) * P.nan
+    OP = P.ones( ( len(x), len(freq) ) ) * P.nan
+    ERR = P.ones( ( len(x), len(freq) ) ) * P.nan
+    
+    for i in range( len( f ) ):
+        #print i, nx[i], nf[i]
+        IP[ nx[i],nf[i] ] = ip[i]
+        OP[ nx[i],nf[i] ] = op[i]
+        ERR[nx[i],nf[i] ] = err[i]
+    
+    return x, freq, coilspacing, IP, OP, ERR    
+
+def importMaxminData( filename, verbose = False ):
+    """ pure import function reading in positions, data, frequencies and geometry """
+    delim = None
+    fid = open(filename)
+    coilspacing = 0.
+    freq = []
+    for i, aline in enumerate( fid ):
+        if aline.split()[0][0].isdigit(): #number found
+            break
+        elif aline.find('COIL') > 0:     #[:6] == '/ COIL':
+            coilspacing = float( aline.replace(':',': ').split()[-2] )
+        elif aline.find('FREQ') > 0:   #[:6] == '/ FREQ':
+            freq = P.array( [float(aa) for aa in aline[aline.find(':')+1:].replace(',',' ').split() if aa[0].isdigit()] )
+    
+    fid.close()
+    
+    if verbose: print "CS=", coilspacing, "F=", freq
+    if aline.find(',')>0: delim=','
+    
+    nf = len( freq )
+    if verbose: print "delim=", delim, "nf=", nf
+    A = P.loadtxt( filename, skiprows=i, delimiter=delim ).T
+    x, y, IP, OP = A[0], A[1], A[2:nf*2+2:2].T, A[3:nf*2+2:2].T
+    if max(x)==min(x):
+        x = y
+
+    return x, freq, coilspacing, IP, OP
+
+def xfplot( ax, DATA, x, freq, everyx=5, orientation='vertical', aspect=40 ):
+    """ plots a matrix according to x and frequencies """
+    nt = range( 0, len( x ), everyx )
+    P.imshow( DATA.T, interpolation='nearest' )
+    P.ylim(P.ylim()[::-1])
+    ax.set_xticks(nt)
+    ax.set_xticklabels(["%g" % xi for xi in x[nt]])
+    ax.set_yticks(range(0,len(freq)+1,2))
+    ax.set_yticklabels(["%g" % freq[i] for i in range(0,len(freq),2)])
+    P.colorbar(orientation=orientation,aspect=aspect)
+    P.xlabel('x [m]')
+    P.ylabel('f [Hz]')
+
+class FDEM2dFOP( g.ModellingBase ):
+    def __init__( self, data, nlay=2, verbose=False ):
+        g.ModellingBase.__init__( self, verbose )
+        self.nlay_ = nlay
+        self.FOP_  = data.FOP( nlay )
+        self.nx_   = len( data.x )
+        self.nf    = len( data.freq() )
+        self.mesh_ = g.createMesh1D( self.nx_, 2*nlay-1 )
+        self.setMesh( self.mesh_ )
+
+    def response( self, model ):
+        modA = P.asarray( model ).reshape( (self.nlay_*2-1,self.nx_) ).T
+        resp = g.RVector( 0 )
+        for modi in modA:
+            resp = g.cat( resp, self.FOP_.response( modi ) )
+        
+        return resp
+    
+class FDEMData():
+    def __init__( self, filename = None):
+        """ initialize data class and load data """
+        self.x, self.f, self.cs, self.IP, self.OP, self.ERR = None,None,None,None,None,None
+        self.height = 1.0
+        
+        if filename:
+            # check if filename extension is TXT or CSV
+            if filename.lower().rfind( '.txt' ) > 0 or filename.lower().rfind( '.csv' ) > 0:
+                self.x, self.f, self.cs, self.IP, self.OP, self.ERR = importEmsysAsciiData( filename )
+            else:
+                self.x, self.f, self.cs, self.IP, self.OP = importMaxminData( filename )
+            
+        self.activeFreq = ( self.f > 0.0 )
+
+    def __repr__( self ):
+        if isinstance( self.x, float ):
+            return "<FDEMdata: sounding with %d frequencies, coilspacing is %.1f>" % (len(self.f), self.cs)
+        else:
+            return  "<FDEMdata: %d soundings with each %d frequencies, coilspacing is %.1f>" % (len(self.x), len(self.f), self.cs)
+        
+    def showInfos( self ): # only for old scripts using it
+        print __repr__( self )
+
+    def deactivate( self, fr ):
+        """ deactivate a single frequency """
+        fi = P.find( P.absolute( self.f / fr - 1.) < 0.1 )
+        self.activeFreq[ fi ] = False
+        
+    def freq( self ):
+        """ return active frequencies """
+        return self.f[ self.activeFreq ]
+    
+    def FOP( self, nlay = 2 ):
+        """ retrieve forward modelling operator using a block discretization """
+        return FDEM1dModelling( nlay, asvector( self.freq() ), self.cs, -self.height )
+    
+    def FOPsmooth( self, zvec ):
+        """ retrieve forward modelling operator using fixed layers (smooth inversion) """
+        return FDEM1dRhoModelling( asvector( zvec ), asvector( self.freq() ), self.cs, -self.height )
+    
+    def selectData( self, xpos=0 ):
+        """ retrieve inphase and outphase vector from index or near given position """
+        if isinstance( xpos, int ) and ( xpos < len( self.x ) ) and ( xpos >= 0 ): # index
+            n = xpos
+        else:
+            n = P.argmin( P.absolute( self.x - xpos ) )
+        
+        if self.ERR is not None:
+            return self.IP[ n, self.activeFreq ], self.OP[ n, self.activeFreq ], self.ERR[ n, self.activeFreq ]
+        else:
+            return self.IP[ n, self.activeFreq ], self.OP[ n, self.activeFreq ], None
+
+    def error( self, xpos=0 ):
+        """ return error vector """
+        ip, op, err = selectData( xpos )
+        return err
+
+    def datavec( self, xpos=0 ):
+        """ extract data vector (stacking inphase and outphase """
+        ip, op, err = self.selectData( xpos )
+        return asvector( P.hstack( ( ip, op ) ) )
+    
+    def errorvec( self, xpos=0, minvalue=0.0 ):
+        """ extract error vector """
+        ip, op, err = self.selectData( xpos )
+        return asvector( P.tile( P.maximum( err * 0.7071, minvalue ), 2 ) )
+    
+    def invBlock( self, xpos=0, nlay=2, noise=1.0, stmod=30., lam=100., lBound=1., uBound=0., verbose=False ):
+        """ yield gimli inversion instance for block inversion """
+        """ inv(xpos,nlay) where nlay can be a FOP or a number of layers """
+        self.transThk = RTransLog()
+        self.transRes = RTransLogLU( lBound, uBound )
+        self.transData = RTrans()
+        # EM forward operator
+        if isinstance( nlay, FDEM1dModelling ):
+            self.fop = nlay
+        else:
+            self.fop = self.FOP( nlay )
+        
+        data = self.datavec( xpos )
+        
+        self.fop.region(0).setTransModel( self.transThk )
+        self.fop.region(1).setTransModel( self.transRes )
+        
+        if isinstance( noise, float ):
+            noiseVec = RVector( len(data), noise)
+        else:
+            noiseVec = asvector( noise )
+        
+        # independent EM inversion
+        self.inv = RInversion( data, self.fop, self.transData, verbose )
+        if isinstance( stmod, float): # real model given
+            model = RVector( nlay * 2 - 1, stmod )
+            model[0] = 2.
+        else:
+            if len( stmod ) == nlay*2-1:
+                model = asvector( stmod )
+            else:
+                model = RVector( nlay*2-1, 30. )
+        
+        self.inv.setAbsoluteError( noiseVec )
+        self.inv.setLambda( lam )
+        self.inv.setMarquardtScheme( 0.8 )
+        self.inv.setDeltaPhiAbortPercent( 0.5 )
+        self.inv.setModel( model )
+        self.inv.setReferenceModel( model )
+        return self.inv
+    
+    def plotData( self, xpos=0, response = None, error=None, ax=None, marker='bo-', rmarker='rx-', clf=True, addlabel='', nv=2 ):
+        """ plot data as curves at given position """
+        ip, op, err = self.selectData( xpos )
+        if error is not None and err is not None:
+            error = err
+        
+        fr = self.freq()
+        if ax is None:
+            if clf: P.clf()
+            P.subplot(1,nv,nv-1)
+        else:
+            P.sca( ax[0] )
+        
+        markersize = 4
+        if error is not None:
+            markersize = 2
+        
+        P.semilogy( ip, fr, marker, label='obs'+addlabel, markersize=markersize )
+        if error is not None and len(error) == len( ip ):
+            P.errorbar( ip, fr, xerr=error )
+        
+        P.axis('tight')
+        if error is not None:
+            P.ylim((min(fr)*.98,max(fr)*1.02))
+
+
+        P.grid(True)
+        P.xlabel('inphase [%]')
+        P.ylabel('f [Hz]')
+        if response is not None:
+            rip = P.asarray( response )[:len(ip)]
+            P.semilogy( rip, fr, rmarker, label='syn'+addlabel )
+        
+        P.legend( loc='best' )
+        
+        if ax is None:
+            P.subplot(1,nv,nv)
+        else:
+            P.sca( ax[1] )
+        
+        P.semilogy( op, fr, marker, label='obs'+addlabel, markersize=markersize )
+        if error is not None and len(error) == len( ip ):
+            P.errorbar( op, fr, xerr=error )
+
+        if response is not None:
+            rop = P.asarray( response )[len(ip):]
+            P.semilogy( rop, fr, rmarker, label='syn'+addlabel )
+        
+        P.axis('tight')
+        if error is not None:
+            P.ylim((min(fr)*.98,max(fr)*1.02))
+        
+        P.grid(True)
+        P.xlabel('outphase [%]')
+        P.ylabel('f [Hz]')
+        P.legend( loc='best' )
+        P.subplot( 1, nv, 1 )
+        return 
+
+        
+    def plotDataOld( self, xpos=0, response = None, marker='bo-', rmarker='rx-', clf=True ):
+        """ plot data as curves at given position """
+        ip, op = self.selectData( xpos )
+        fr = self.freq()
+        if clf: P.clf()
+        P.subplot(121)
+        P.semilogy( ip, fr, marker, label='obs' )
+        P.axis('tight')
+        P.grid(True)
+        P.xlabel('inphase [%]')
+        P.ylabel('f [Hz]')
+        if response is not None:
+            rip = P.asarray( response )[:len(ip)]
+            P.semilogy( rip, fr, rmarker, label='syn' )
+        
+        P.legend( loc='best' )
+        
+        P.subplot(122)
+        P.semilogy( op, fr, marker, label='obs' )
+        if response is not None:
+            rop = P.asarray( response )[len(ip):]
+            P.semilogy( rop, fr, rmarker, label='syn' )
+        
+        P.axis('tight')
+        P.grid(True)
+        P.xlabel('outphase [%]')
+        P.ylabel('f [Hz]')
+        P.legend( loc='best' )
+        P.show()
+        return
+    
+    def showModelAndData( self, model, xpos=0, response=None ):
+        P.clf()
+        model = P.asarray( model )
+        nlay = ( len( model ) + 1 ) / 2
+        thk = model[:nlay-1]
+        res = model[nlay-1:2*nlay-1]
+        ax1 = P.subplot(131)
+        draw1dmodel( res, thk )
+        ax2 = P.subplot(132)
+        ax3 = P.subplot(133)
+        self.plotData( xpos, response, (ax2, ax3), clf=False )
+         
+    def plotAllData( self, allF = True, orientation='vertical', outname=None ):
+        """ plot data along a profile as image plots for IP and OP """
+        
+        freq = self.freq()
+        nf = len( freq )
+        np = 2
+        if self.ERR is not None:
+            np = 3
+        
+        P.clf()
+        ax1 = P.subplot(np,1,1)
+        xfplot( ax1, self.IP[:,self.activeFreq], self.x, freq, orientation=orientation )
+        P.title('inphase percent')
+        ax2 = P.subplot(np,1,2)
+        xfplot( ax2, self.OP[:,self.activeFreq], self.x, freq, orientation=orientation )
+        P.title('outphase percent')
+        if self.ERR is not None:
+            ax3 = P.subplot(np,1,3)
+            xfplot( ax3, self.ERR[:,self.activeFreq], self.x, freq, orientation=orientation )
+            P.title('error percent')
+
+        if outname is not None:
+            P.savefig( outname )
+        
+        P.show()
+        return
+
+    def plotModelAndData( self, model, xpos, response, modelL=None, modelU=None ):
+        plotData( xpos, response, nv=3 )
+        draw1dmodel( model, color='blue' )
+        if modelL is not None and modelU is not None:
+            draw1dmodelErr( model, modelL, modelU )
+
+        return
+    
+    def FOP2d( self, nlay ):
+        """ 2d forward modelling operator """
+        return FDEM2dFOP( self, nlay )
+    
+    def inv2D( self, nlay, lam=100., resL=1., resU=1000., thkL=1., thkU=100., minErr = 1.0 ):
+        """ 2d LCI inversion class """
+        if isinstance( nlay, int ):
+            modVec = g.RVector( nlay*2 -1, 30. )
+            cType = 0 # no reference model
+        else:
+            modVec = nlay
+            cType = 10 # use this as referencemodel
+            nlay = ( len( modVec ) + 1 ) / 2
+
+
+        # init forward operator
+        self.f2d = self.FOP2d( nlay)
+
+        # transformations
+        self.tD = g.RTrans()
+        self.tThk = g.RTransLogLU( thkL, thkU )
+        self.tRes = g.RTransLogLU( resL, resU )
+        for i in range( nlay-1 ): self.f2d.region( i ).setTransModel( self.tThk )
+        for i in range( nlay-1, nlay*2-1 ): self.f2d.region( i ).setTransModel( self.tRes )
+        
+        # set constraints
+        self.f2d.region( 0 ).setConstraintType(10)
+        self.f2d.region( 1 ).setConstraintType(10)
+
+        # collect data vector
+        datvec = g.RVector( 0 )
+        for i in range( len( self.x ) ):
+            datvec = g.cat( datvec, self.datavec( i ) )
+
+        # collect error vector
+        errVec = []
+        for i in range( len( self.x ) ):
+            errVec.extend( P.maximum( self.ERR[i][ self.activeFreq ] * 0.701, minErr ) )
+            errVec.extend( P.maximum( self.ERR[i][ self.activeFreq ] * 0.701, minErr ) )
+
+        # generate starting model by repetition
+        model = g.asvector( P.repeat( modVec, len(self.x) ) )
+        INV = g.RInversion( datvec, self.f2d, self.tD ) 
+        INV.setAbsoluteError( g.asvector( errVec ) )
+        INV.setLambda( lam )
+        INV.setModel( model )
+        INV.setReferenceModel( model )
+        return INV
+
+if __name__ == "__main__":
+    import sys
+    from optparse import OptionParser
+
+    parser = OptionParser( "usage: %prog [options] fdem", version="%prog: " + g.versionStr()  )
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true"
+                            , help="be verbose", default=False )
+    
+    (options, args) = parser.parse_args()
+
+    if options.verbose:
+        __verbose__ = True 
+        
+    if len( args ) == 0:
+        parser.print_help()
+        print "Please add a mesh or model name."
+        sys.exit( 2 )
+    else:
+        if len( args ) == 0:
+            parser.print_help()
+            print "Please add a mesh or model name."
+            sys.exit( 2 )
+        else:
+            fdem = FDEMData( args[ 0 ] )
+
+
---
+++ python/pygimli/physics/em/tdem.py
@@ -0,0 +1,213 @@
+import pylab as P
+from pygimli import FDEM1dModelling, RVector, asvector, RTrans, RTransLog, RTransLogLU, RInversion
+from pygimli.utils import draw1dmodel
+from math import sqrt, pi
+
+def rhoafromU( UbyI, t, Tx, Rx=None ):
+    ''' apparent resistivity curve from classical TEM '''
+    if Rx is None:
+        Rx = Tx # assume single/coincident loop
+    
+    mu0 = 4e-7 * pi
+#    rhoa = ( Rx * Tx * mu0 / 20. / UbyI )**(2./3.) * t**(-5./3.) * mu0 / pi
+    rhoa = ( Rx * Tx * mu0 / 20. / UbyI )**(2./3.) * t**(-5./3.) * mu0 / pi
+    return rhoa
+
+def rhoafromB( B, t, Tx, I=1 ):
+    ''' apparent resistivity from B-field TEM '''
+    mu0 = 4e-7 * pi
+    rhoa = ( I * Tx * mu0 / 30. / B )**(2./3.) * mu0 / pi / t
+    return rhoa
+
+def rhoa( snd, cal=260e-9, corrramp=True ):
+    ''' compute apparent resistivity from sounding '''
+    Tx = P.prod( [float(a) for a in snd['LOOP_SIZE'].split()] )
+    if snd.has_key('COIL_SIZE'):
+        Rx = snd['COIL_SIZE']
+    else:
+        Rx = Tx
+    
+    v = snd['VOLTAGE']
+    istart, istop = 0, len(v) # default: take all
+    mav = P.find( v == max(v) )
+    if len(mav) > 1: #several equal big ones: start after
+        istart = max(mav)+1
+
+    if min(v) < 0.0: # negative values: stop at first
+        istop = min( P.find( v < 0.0 ) )
+    
+    v = v[istart:istop]
+    dv = snd['ST_DEV'][istart:istop] #/ snd['CURRENT']
+    t = snd['TIME'][istart:istop]
+    if corrramp and snd.has_key('RAMP_TIME'): 
+        t = t - snd['RAMP_TIME']
+    
+    if Rx == 1: # apparently B-field
+        rhoa = rhoafromB( v * cal, t, Tx )
+    else:
+        rhoa = rhoafromU( v, t, Tx, Rx )
+
+    rhoaerr = dv / v * (2./3.)
+    return rhoa, t, rhoaerr
+
+def readusffile( filename ):
+    ''' read data from single USF (universal sounding file) file
+        DATA = readusffile( filename )
+        DATA = readusffile( filename, DATA ) will append to DATA '''
+    
+    DATA = []
+    columns = []
+    nr = 0
+    sounding = {}
+    sounding['FILENAME'] = filename
+    isdata = False
+    fid = open( filename )
+    for line in fid:
+        zeile = line.replace('\n','').replace(',','') # commas are useless here
+        if zeile: # anything at all
+            if zeile[0] == '/': # comment-like
+                if zeile[1:4] == 'END': # end of a sounding
+                    if isdata: # already read some data
+                        sounding[ 'data' ] = columns
+                        for i, cn in enumerate( sounding['column_names'] ):
+                            sounding[cn] = columns[:,i]
+                        
+                        DATA.append( sounding )
+                        sounding = {}
+                    
+                    isdata = not isdata # turn off data mode
+                elif zeile.find(':') > 0: # key-value pair
+                    key, value = zeile[1:].split(':')
+                    try:
+                        val = float( value )
+                        sounding[key] = val
+                    except:    
+                        sounding[key] = value
+    
+            else:
+                if isdata:
+                    values = zeile.split()
+                    try:
+                        for i, v  in enumerate( values ):
+                            columns[ nr, i ] = float( v )
+                        
+                        nr += 1
+                    except:
+                        sounding['column_names'] = values
+                        columns = P.zeros( ( int(sounding['POINTS']), len( values ) ) )
+                        nr = 0
+    
+    fid.close()
+    return DATA
+
+def readusffiles( filenames, DATA = [] ):
+    ''' read all soundings data from a list of usf files
+        DATA = readusffiles( filenames ) '''
+    ALLDATA = []
+    for onefile in filenames:
+        ALLDATA.extend( readusffile( onefile ) )
+    
+    return ALLDATA
+
+def readSiroTEMData( fname ):
+    ''' read TEM data from siroTEM instrument dump 
+        DATA = readSiroTEMData( filename )
+        .. list of soundings with USF and siro-specific keys 
+    '''
+    Time_ST = P.array( [487.,887.,1287.,1687.,2087.,2687.,3487.,4287.,5087.,5887.,7087.,8687.,10287.,11887.,13487.,
+                    15887.,19087.,22287.,25487.,28687.,33487.,39887.,46287.,52687.,59087.,68687.,81487.,94287.,
+                    107090.,119890.,139090.,164690.,190290.,215890.,241490.,279890.,331090.,382290.,433490.,
+                    484690.,561490.,663890.,766290.,868690.,971090.,1124700.,1329500.,1534300.,1739100.,1943900.] )
+    Time_ET = P.array( [0.05, 0.1, 0.15, 0.25, 0.325, 0.425, 0.525, 0.625, 0.725, 0.875, 1.075, 1.275, 1.475, 1.675,
+                        1.975, 2.375, 2.775, 3.175, 3.575, 4.175, 4.975, 5.775, 6.575, 7.375, 8.575, 10.175, 11.775,
+                        13.375, 14.975, 17.375, 20.575, 23.775, 26.975, 30.175, 34.975, 41.375, 47.775, 54.175, 60.574,
+                        70.175, 82.975, 95.775,108.575,121.375,140.575,166.175,191.775,217.375,242.975,281.375,332.575] )
+    
+    fid = open( fname )
+    # read in file header until : sign
+    line = 'a'
+    while len( line ) > 0 and line[0] != ':':
+        line = fid.readline()
+
+    DATA = []
+    line = fid.readline()
+    while line[0] != ';':
+        header = line[1:-6].split(',')
+
+        snd = {} # dictionary, uppercase corresponds to USF format keys
+        snd['INSRTUMENT'] = 'siroTEM'
+        snd['dtype'] = int(header[3])
+        dstring = header[1]
+        snd['DATE'] = int( '20' + dstring[6:8] + dstring[3:4] + dstring[0:1] )
+        snd['win0'], snd['win1'], ngain, snd['conf'], snd['nch'], snd['SOUNDING_NUMBER']  = \
+            [int(h) for h in header[5:11]]
+        
+        snd['GAIN_FACTOR'] = [ 0.1, 1.0, 10.0, 100.0 ][ ngain ] # predefined gain factors
+        snd['STACK_SIZE'] = int( header[14] )
+        snd['ttype'] = int( header[20] ) # 1-composite,2-earlytime,3-standard,4-highresolution
+        snd['CURRENT'], snd['RAMP_TIME'] = float( header[17] ), float( header[18] )*1e-6
+        snd['TIME_DELAY'] = float( header[19] )
+        snd['LOOP_SIZE'], snd['COIL_SIZE'] = float( header[21]), float( header[22])
+
+        dummy = fid.readline()
+        data = []
+        line = fid.readline()[:-1] # trim CR+LF newline
+        while len(line) >0:
+            while line[-1] == '/':
+                line = line[:-1] + fid.readline()[:-1].replace('\t','')
+                aline = line
+
+            nums = [float(el[-7:-2])*10**(float(el[-2:])) for el in line[1:-5].split(',')[1:]]
+            data.append( P.array( nums ) )
+            line = fid.readline()[:-1] # trim newline
+
+        snd['VOLTAGE'] = data[0]
+        if snd['ttype'] == 2: # early time
+            snd['TIME'] = Time_ET[ snd['win0']-1:snd['win1'] ] * 1e-3
+        if snd['ttype'] == 3: # standard time
+            snd['TIME'] = Time_ST[ snd['win0']-1:snd['win1'] ] * 1e-6
+        
+        snd['ST_DEV'] = data[1]
+        if snd['dtype'] >0: #normal measurement
+            DATA.append( snd )
+
+        line = fid.readline()
+
+    fid.close() 
+    return DATA
+
+class TDEMData():
+    """ TEM class holding data etc. """
+    def __init__( self, filename, height=1.0, verbose=False ):
+        """ initialize data class and load data """
+
+    def __init__( self, filename = None):
+        """ initialize data class and load data """
+        self.x, self.f, self.cs, self.IP, self.OP, self.ERR = None,None,None,None,None,None
+        self.height = 1.0
+        
+        if filename:
+            # check if filename extension is usf
+            if filename.lower().rfind( '.usf' ) > 0:
+                if filename.find( '*' ) > 0:
+                    self.DATA = readusffiles( filename )
+                else:
+                    self.DATA = readusffile( filename )
+            elif filename.lower().rfind( '.txt' ) > 0:
+                self.DATA = readSiroTEMData( filename )
+            
+        self.activeFreq = ( self.f > 0.0 )
+
+    def __repr__( self ):
+        if isinstance( self.x, float ):
+            return "<FDEMdata: sounding with %d frequencies, coilspacing is %.1f>" % (len(self.f), self.cs)
+        else:
+            return  "<FDEMdata: %d soundings with each %d frequencies, coilspacing is %.1f>" % (len(self.x), len(self.f), self.cs)
+        
+    def showInfos( self ): # only for old scripts using it
+        print __repr__( self )
+
+
+    
+if __name__ == '__main__':
+    print "print do some tests here"
