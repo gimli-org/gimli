12b5adc63810c4b1c4ab00aea2c32a9fa5e5590a
florian-wagner <wagnerfl@student.ethz.ch>
2014-08-28 11:58:17 +0000

    added general load function for data/mesh IO

---
+++ python/pygimli/io/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+================
+Input and Output
+================
+
+GIMLi supports different data formats. This module provides all the necessary
+import and export functionality including a general load and save function with
+automatic filetype detection.
+"""
+
+from .load import *
