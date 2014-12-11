7ffec48b50b726857344ca4f0511e85c93a2a0b4
carsten-forty2 <carsten@gimli.org>
2014-12-11 17:20:40 +0000

    test fix for landscape tests

--- python/pygimli/gui/main/__init__.py
+++ python/pygimli/gui/main/__init__.py
@@ -2,7 +2,9 @@
 
 """ Main gui application. """
 
-from . guiMainFrameQt import QtGui, QtCore
-from . guiMainFrameQt import PyGIMLIApp
-from . guiMainFrameQt import PyGUISystemMainFrame
-from . guiMainFrameQt import WorkSpace
+from . guiMainFrameQt import *
+
+#from . guiMainFrameQt import QtGui, QtCore
+#from . guiMainFrameQt import PyGIMLIApp
+#from . guiMainFrameQt import PyGUISystemMainFrame
+#from . guiMainFrameQt import WorkSpace
