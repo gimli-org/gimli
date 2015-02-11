020ea142a2a19723e9bad4af20e795b53a0f8e76
halbmy <halbmy@gmail.com>
2015-02-11 09:12:01 +0000

    minor corrections on drawTravelTimeData

--- python/pygimli/mplviewer/dataview.py
+++ python/pygimli/mplviewer/dataview.py
@@ -3,8 +3,8 @@
     Some data related viewer
 """
 import pygimli as pg
-
 import numpy as np
+
 
 def drawSensorAsMarker(ax, data):
     '''
@@ -12,61 +12,63 @@ def drawSensorAsMarker(ax, data):
     '''
     elecsX = []
     elecsY = []
-    
+
     for i in range(len(data.sensorPositions())):
         elecsX.append(data.sensorPositions()[i][0])
         elecsY.append(data.sensorPositions()[i][1])
-    
-    electrodeMarker, =  ax.plot(elecsX, elecsY, 'x', color = 'black', picker = 5.)
-    
-    ax.set_xlim([data.sensorPositions()[0][0]-1.,
-                 data.sensorPositions()[data.sensorCount() -1][0] + 1.])
+
+    electrodeMarker, =  ax.plot(elecsX, elecsY, 'x', color='black', picker=5.)
+
+    ax.set_xlim([data.sensorPositions()[0][0] - 1.,
+                 data.sensorPositions()[data.sensorCount() - 1][0] + 1.])
 
     return electrodeMarker
 
+
 def drawTravelTimeData(a, data):
-    """
-        Draw first arrival traveltime data into mpl axes a. 
-        data of type \ref DataContainer must contain sensorIdx 's' and 'g' 
-        and thus numbered internal from [0..n)
-        
-    """
+    '''
+        Draw first arrival traveltime data into mpl axes a.
+        data of type \ref DataContainer must contain sensorIdx 's' and 'g'
+        and thus being numbered internally [0..n)
+    '''
 
     x = pg.x(data.sensorPositions())
-    z = pg.z(data.sensorPositions())
+#    z = pg.z(data.sensorPositions())
 
     shots = pg.unique(pg.sort(data('s')))
     geoph = pg.unique(pg.sort(data('g')))
 
     startOffsetIDX = 0
 
-    if min(min(shots), min(geoph) == 1): 
+    if min(min(shots), min(geoph)) == 1:
         startOffsetIDX = 1
-    
+
     a.set_xlim([min(x), max(x)])
     a.set_ylim([max(data('t')), -0.002])
-        
+    a.figure.show()
+
     for shot in shots:
         gIdx = pg.find(data('s') == shot)
         sensorIdx = [int(i__ - startOffsetIDX) for i__ in data('g')[gIdx]]
         a.plot(x[sensorIdx], data('t')[gIdx], 'x-')
-      
-    yPixel = a.transData.inverted().transform_point((1, 1))[1]-\
+
+    yPixel = a.transData.inverted().transform_point((1, 1))[1] - \
         a.transData.inverted().transform_point((0, 0))[1]
-    xPixel = a.transData.inverted().transform_point((1, 1))[0]-\
+    xPixel = a.transData.inverted().transform_point((1, 1))[0] - \
         a.transData.inverted().transform_point((0, 0))[0]
 
     # draw shot points
-    a.plot(x[[int(i__ - startOffsetIDX) for i__ in shots]], 
-           np.zeros(len(shots)) + 8.*yPixel, 'gv', markersize = 8)    
+    a.plot(x[[int(i__ - startOffsetIDX) for i__ in shots]],
+           np.zeros(len(shots)) + 8.*yPixel, 'gv', markersize=8)
 
     # draw geophone points
-    a.plot(x[[int(i__ - startOffsetIDX) for i__ in geoph]], 
-           np.zeros(len(geoph)) + 3.*yPixel, 'r^', markersize = 8)    
+    a.plot(x[[int(i__ - startOffsetIDX) for i__ in geoph]],
+           np.zeros(len(geoph)) + 3.*yPixel, 'r^', markersize=8)
 
-    a.grid()    
+    a.grid()
     a.set_ylim([max(data('t')), +16.*yPixel])
     a.set_xlim([min(x)-5.*xPixel, max(x)+5.*xPixel])
 
     a.set_xlabel('x-Coordinate [m]')
     a.set_ylabel('Traveltime [ms]')
+# def drawTravelTimeData(...)
