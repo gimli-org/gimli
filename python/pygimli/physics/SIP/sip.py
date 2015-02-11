a6726f15996f44d006e75cd3d5457898e9d0302f
halbmy <halbmy@gmail.com>
2015-02-11 13:42:37 +0000

    fixed import error of sip.py

--- python/pygimli/physics/SIP/sip.py
+++ python/pygimli/physics/SIP/sip.py
@@ -70,5 +70,85 @@ class DebyePhi(pg.ModellingBase):
         return -np.angle(y)
 
 
+def readSIP256file(resfile, verbose=False):
+    """ read SIP256 file (RES format) """
+    activeBlock = ''
+    header = {}
+    LINE = []
+    dataAct = False
+    with open(resfile, 'r') as f:
+        for line in f:
+            if dataAct:
+                LINE.append(line)
+            elif len(line):
+                if line[0] == '[':
+                    token = line[1:line.rfind(']')].replace(' ', '_')
+                    if token == 'Messdaten_SIP256':
+                        dataAct = True
+                    elif token[:3] == 'End':
+                        header[activeBlock] = np.array(header[activeBlock])
+                        activeBlock = ''
+                    elif token[:5] == 'Begin':
+                        activeBlock = token[6:]
+                        header[activeBlock] = []
+                    else:
+                        value = line[line.rfind(']')+1:]
+                        try:  # direct line information
+                            if '.' in value:
+                                num = float(value)
+                            else:
+                                num = int(value)
+                            header[token] = num
+                        except Exception:  # maybe beginning or end of a block
+                            pass
+                else:
+                    if activeBlock:
+                        nums = np.array(line.split(), dtype=float)
+                        header[activeBlock].append(nums)
+
+    DATA, Data, data, AB, RU, ru = [], [], [], [], [], []
+    for line in LINE:
+        sline = line.split()
+        if line.find('Reading') == 0:
+            rdno = int(sline[1])
+            if rdno:
+                AB.append((int(sline[4]), int(sline[6])))
+            if ru:
+                RU.append(ru)
+                ru = []
+            if rdno > 1 and Data:
+                Data.append(np.array(data))
+                DATA.append(Data)
+                Data, data = [], []
+                if verbose:
+                    print('Reading ' + str(rdno - 1) + ':' + str(len(Data)) +
+                          ' RUs')
+        elif line.find('Remote Unit') == 0:
+            ru.append(int(sline[2]))
+            if data:
+                Data.append(np.array(data))
+                data = []
+        elif line.find('Freq') >= 0:
+            pass
+        elif len(sline) > 1 and rdno > 0:  # some data present
+            for c in range(6):
+                if len(sline[c]) > 11:  # too long line / missing space
+                    if c == 0:
+                        part1 = sline[c][:-15]
+                        part2 = sline[c][10:]
+                    else:
+                        part1 = sline[c][:-10]
+                        part2 = sline[c][9:]
+                    sline = sline[:c] + [part1] + [part2] + sline[c+1:]
+            data.append(np.array(sline[:5], dtype=float))
+
+    Data.append(np.array(data))
+    DATA.append(Data)
+    if verbose:
+        print('Reading ' + str(rdno) + ':' + str(len(Data)) + ' RUs')
+
+    return header, DATA, AB, RU
+
+
 if __name__ == "__main__":
     pass
