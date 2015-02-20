59f7893932387de7b59961458868732e33422499
carsten-forty2 <carsten.ruecker@tu-berlin.de>
2015-02-20 15:49:38 +0000

    fix jenkins complains

--- doc/examples/inversion/develop/plot_ert_simple-2d.py
+++ doc/examples/inversion/develop/plot_ert_simple-2d.py
@@ -24,6 +24,7 @@ inv = pg.RInversion(data("rhoa"), fop, verbose=True, dosave=True)
 datTrans = pg.RTransLog()
 modTrans = pg.RTransLog()
 
+inv.setMaxIter(1)
 inv.setTransData(datTrans)
 inv.setTransModel(modTrans)
 inv.setError(data('err'))
@@ -33,17 +34,37 @@ inv.setLambda(5)
 
 model = inv.run()
 
-C = fop.contraintsMatrix()
-S = fop.jacobian()
+#C = fop.contraintsMatrix()
+#S = fop.jacobian()
 
 modelMesh = fop.regionManager().paraDomain()
 pg.showLater(1)
 
+ax, cbar = pg.show(modelMesh, model)
+
 pg.show(modelMesh, data.sensorPositions(), axes=ax, showLater=1)
 pg.show(modelMesh, axes=ax)
 
-ax, cbar = pg.show(modelMesh, model)
+
 ax, cbar = pg.show(modelMesh, model, tri=1, interpolate=1)
 ax, cbar = pg.show(modelMesh, model, tri=1, interpolate=0, shading = 'gouraud')
+
+potmatrix = fop.solution()
+print(potmatrix)
+print(potmatrix[0])
+
+ax, cbar = pg.show(fop.mesh(), pg.logTransDropTol(potmatrix[0]-potmatrix[1], 1e-5))
+
+
+u = potmatrix[0]-potmatrix[1]
+
+uc = pg.RVector(mesh.cellCount())
+for c in mesh.cells():
+    uc[c.id()] = c.pot(c.center(), u)
+
+ax, cbar = pg.show(mesh, pg.logTransDropTol(uc,1e-5))
+ax, cbar = pg.show(mesh, pg.logTransDropTol(uc,1e-5), tri=1, interpolate=0 )
+ax, cbar = pg.show(mesh, pg.logTransDropTol(uc,1e-5), tri=1, interpolate=1, nLevs=256, omitLines=1)
+ax, cbar = pg.show(mesh, pg.logTransDropTol(uc,1e-5), tri=1, shading = 'gouraud')
 
 pg.showNow()
