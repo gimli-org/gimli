093fac8d3135f10d179686b279fe7d26a066d302
halbmy <halbmy@gmail.com>
2015-03-05 23:12:49 +0000

    checks for pyproj/osgeo packages in gps and fdem, some text and pep corrections in tutorial

--- doc/tutorials/inversion/plot-1-polyfit.py
+++ doc/tutorials/inversion/plot-1-polyfit.py
@@ -5,8 +5,9 @@
 Polyfit
 -------
 
-This tutorial shows how to treat the simplest inversion case with an own forward calculation.
-As linear regression is the easiest inverse problem we start with fitting a polynomial of degree :math:`P`
+This tutorial shows how to treat the simplest inversion case with an own
+forward calculation. As regression is the easiest inverse problem we start with
+fitting a polynomial of degree :math:`P`
 
 .. math::
 
@@ -14,28 +15,36 @@ As linear regression is the easiest inverse problem we start with fitting a poly
 
 to some existing data :math:`y`.
 The unknown model is the coefficient vector :math:`\m=[p_0,\ldots,p_P]`.
-The vectorized function for a vector :math:`\arr{x}=\transpose{[x_1,\ldots,x_N]}` can be written as matrix-vector product
+The vectorized function for a vector
+:math:`\arr{x}=\transpose{[x_1,\ldots,x_N]}`
+can be written as matrix-vector product
 
 .. _eq:yAx:
 .. math::
 
   \f(\arr{x}) = \A \arr{x} \quad\mbox{with}\quad \A=\left[ \begin{array}{cccc}
-  1 & x_1 & \ldots & x_1^P \\ \vdots & \vdots & \ddots & \vdots \\ 1 & x_N & \ldots & x_N^P
-  \end{array} \right] = [ {\bf 1}\quad \arr{x} \quad \arr{x}^2 \ldots \arr{x}^P ] \;.
+  1 & x_1 & \ldots & x_1^P \\
+  \vdots & \vdots & \ddots & \vdots \\ 1 & x_N & \ldots & x_N^P
+  \end{array} \right] =
+  [ {\bf 1}\quad \arr{x} \quad \arr{x}^2 \ldots \arr{x}^P ] \;.
 
-We set up the modelling operator, i.e. to return :math:`\f(\arr{x})` for given :math:`p_i`, as a class derived from the modelling base class.
-The latter holds the main mimic of generating Jacobian, gradients by brute force.
-The only function to overwrite is \cw{response()}.
+We set up the modelling operator, i.e. to return :math:`\f(\arr{x})` for given
+:math:`p_i`, as a class derived from the modelling base class.
+The latter holds the main mimic of generating Jacobian, gradients by brute
+force. The only function to overwrite is \cw{response()}.
 
-Python is a very flexible language for programming and scripting and has many packages for numerical computing and graphical visualization.
+Python is a very flexible language for programming and scripting and has many
+packages for numerical computing and graphical visualization.
 For this reason, we built Python bindings and compiled the library pygimli.
 As a main advantage, all classes can be used and derived.
 This makes the use of GIMLi very easy for non-programmers.
-All existing modelling classes can be used, but it is also easy to create new modelling classes.
+All existing modelling classes can be used, but it is also easy to create new
+modelling classes.
 
 We exemplify this by the preceding example.
 First, the library must be imported.
-To avoid name clashes with other libraries we suggest to import `pygimli` and alias it to an easy name, e.g. by using
+To avoid name clashes with other libraries we suggest to import `pygimli` and
+alias it to an easy name (as usually done for numpy or matplotlib), e.g. by
 """
 
 import pygimli as pg
@@ -45,26 +54,30 @@ import matplotlib.pyplot as plt
 print(pg.__version__)
 
 """
-#! As a result, all :ref:`sec:api` objects (classes and functions) can be referred to with a preceding `pg.`, e.g.,
-#! printing the version string for gimli.
+As a result, all :ref:`sec:api` objects (classes and functions) can be referred
+to with a preceding `pg.`, e.g., printing the version string for gimli.
 
+Next, the modelling class is derived from ModellingBase, a constructor is
+defined and the response function is defined.
 
-Next, the modelling class is derived from ModellingBase, a constructor is defined and the response function is defined.
-
-The pygimli library must once be imported (in this case under the name g) and all classes (e.g. modelling operators)
-can be used by pg.classname, e.g. pg.RVector is the already known vector of real (double) values.
+The pygimli library must once be imported (in this case under the name g) and
+all classes (e.g. modelling operators) can be used by pg.classname, e.g.
+pg.RVector is the already known vector of real (double) values.
 
 The main program is very easy then and the code is very similar to C++.
 Data are loaded, both forward operator and inversion are created.
 Inversion options are set and it the result of run is save to a file.
 That's it.
 
-As a main advantage of Python, the actual computations can be easily combined with post-processing or visualization, even building graphical user-interfaces.
-In this code example we use matplotlib, a plotting library inside of pylab, a compound of different routines for numerics and plotting, very much comparable to MatLab.
+As a main advantage of Python, the actual computations can be easily combined
+with post-processing or visualization, even building graphical user-interfaces.
+In this code example we use matplotlib, a plotting library inside of pylab, a
+compound of different routines for numerics and plotting, comparable to MatLab.
 """
 
+
 class FunctionModelling(pg.ModellingBase):
-    def __init__(self, nc, xvec, verbose = False):
+    def __init__(self, nc, xvec, verbose=False):
         pg.ModellingBase.__init__(self, verbose)
         self.x_ = xvec
         self.nc_ = nc
@@ -72,32 +85,24 @@ class FunctionModelling(pg.ModellingBase):
 
     def response(self, par):
         y = pg.RVector(self.x_.size(), par[0])
-
-        for i in range( 1, self.nc_ ):
-            y += pg.pow(self.x_, i) * par[i];
-        return y;
+        for i in range(1, self.nc_):
+            y += pg.pow(self.x_, i) * par[i]
+        return y
 
     def startModel(self):
         return pg.RVector(self.nc_, 0.5)
 
 
-##!this is doku within
+"""
+Let us create some synthetic data for some x values
+"""
 
-
-## evaluate f(x) = 1.1 + 2.1 * x
 x = np.arange(0., 10., 1)
-
-#"""
-#this is doku within
-#"""
-
 y = 1.1 + 2.1 * x
 
-#"""
-#this is doku within
-#"""
-
-#print((x, y))
+"""
+We now start by setting up the modelling operator, and inversion and run it.
+"""
 
 nP = 3
 
@@ -107,22 +112,27 @@ fop = FunctionModelling(nP, pg.asvector(x))
 # initialize inversion with data and forward operator and set options
 inv = pg.RInversion(y, fop)
 
-## constant absolute error of 0.01 is 1% (not necessary, only for chi^2)
+# constant absolute error of 0.01 is 1% (not necessary, only for chi^2)
 inv.setAbsoluteError(0.01)
 
-## the problem is well-posed and does not need regularization
+# the problem is well-posed and does not need regularization
 inv.setLambda(0)
 
-## actual inversion run yielding coefficient model
+# actual inversion run yielding coefficient model
 coeff = inv.run()
+
+print(coeff)
+
+"""
+The result is easily plotted by
+"""
 
 plt.plot(x, y, 'rx', x, inv.response(), 'b-')
 
 """
-create output
 
 .. image:: PLOT2RST.current_figure
 
 """
 
-#plt.show()
+plt.show()
--- doc/tutorials/modelling/plot_2-mod-fem.py
+++ doc/tutorials/modelling/plot_2-mod-fem.py
@@ -1,40 +1,42 @@
 #!/ussr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    
+
 Modelling
 ---------
 
-This is the first step for the modelling tutorial where we actually use 
-finite elements computation. 
+This is the first step for the modelling tutorial where we actually use
+finite elements computation.
 
-We will not go in deep detail about the finite elements theory here, 
+We will not go in deep detail about the finite elements theory here,
 as this can be found in several books, e.g., :cite:`Zienkiewicz1977`
 
-In this modelling tutorial we just want to solve some simple problems 
+In this modelling tutorial we just want to solve some simple problems
 to show how the *M* (Modelling) in *GIMLi* works.
 
-We start with a simple elliptic partial differential equation and 
+We start with a simple elliptic partial differential equation and
 with zero boundary values, but a nonzero right hand side.
 
 .. math::
 
-    \nabla\cdot(A \cdot \nabla u ) + B u + C & = 0 \quad{\mathrm{in}}\quad\Omega \\
-    \alpha u + \beta \frac{\partial u }{\partial n} & = 0 \quad{\mathrm{on}}\quad\partial\Omega
-   
-By letting :math:`A=1,\,B=0\,` and :math:`C = 1` we get the simplest Poisson equation:
-   
+    \nabla\cdot(A \cdot \nabla u ) + B u + C & =
+    0 \quad{\mathrm{in}}\quad\Omega \\
+    \alpha u + \beta \frac{\partial u }{\partial n} & =
+    0 \quad{\mathrm{on}}\quad\partial\Omega
+
+Let :math:`A=1,\,B=0\,` and :math:`C = 1` we get the simplest Poisson equation:
+
 .. math::
 
     - \Delta u & = 1 \quad{\mathrm{in}}\quad\Omega\\
-               u & = 0 \quad{\mathrm{on}}\quad\partial\Omega\\
-  
+             u & = 0 \quad{\mathrm{on}}\quad\partial\Omega\\
+
 
 Model domain is the unit square: :math:`\Omega=[-1, 1]^2`
 
-We start by importing  the pygimli package 
-Additionally we import the default solver that performs finite element solution 
-plus our generalized viewer routine. 
+We start by importing  the pygimli package
+Additionally we import the default solver that performs finite element solution
+plus our generalized viewer routine.
 """
 
 import pygimli as pg
@@ -42,8 +44,8 @@ import pygimli as pg
 import numpy as np
 import matplotlib.pyplot as plt
 
-""" 
-We create a grid for our modelling domain with equidistant spacing in x 
+"""
+We create a grid for our modelling domain with equidistant spacing in x
 and y direction.
 """
 
@@ -51,39 +53,36 @@ grid = pg.createGrid(x=np.linspace(-1.0, 1.0, 10),
                      y=np.linspace(-1.0, 1.0, 10))
 
 """
-Now we can call the solver :py:mod:`pygimli.solver.solver.solve`  for some default material values and global 
-homogeneous Dirichlet boundary conditions.
+Now we can call the solver :py:mod:`pygimli.solver.solver.solve`  for some
+default material values and global homogeneous Dirichlet boundary conditions.
 """
 
 u = pg.solver.solve(grid, f=1.,
-                    uBoundary=[grid.findBoundaryByMarker(1,5), 0.0],
+                    uBoundary=[grid.findBoundaryByMarker(1, 5), 0.0],
                     verbose=True)
 
 """
-.. error::
-
-    Do we find an analytical solution for this?
-
-The result is drawn with the function :py:mod:`pygimli.viewer.showmesh.show` 
+The result is drawn with the function :py:mod:`pygimli.viewer.showmesh.show`
 """
+
 
 def uAna(r):
     x = r[0]
     y = r[1]
-   
+
     ret = 0
     for k in range(1, 151, 2):
         kp = k*np.pi
         s = np.sin(kp * (1. + x)/2) / (k**3 * np.sinh(kp)) * \
             (np.sinh(kp * (1. + y)/2) + np.sinh(kp * (1. - y)/2))
-        #print(k, s)
+#        print(k, s)
         ret += s
-    return (1. - x**2)/2 - 16./(np.pi**3) *ret
+    return (1. - x**2)/2 - 16./(np.pi**3) * ret
 
 ax, cbar = pg.show(grid, data=u, colorBar=True, label='P1 Solution $u$',
                    showLater=True)
 """
-Show is just a shortcut for various drawing routines, that can also be called directly.
+Show is just a shortcut for various routines that can also be called directly.
 
 """
 pg.mplviewer.drawMesh(ax, grid)
@@ -99,22 +98,25 @@ grid.
 gridh2 = grid.createH2()
 
 uh = pg.solver.solve(gridh2, f=1.,
-                     uBoundary=[gridh2.findBoundaryByMarker(1,5), 0.0],
+                     uBoundary=[gridh2.findBoundaryByMarker(1, 5), 0.0],
                      verbose=True)
 
-ax,cbar = pg.show(gridh2, data=uh, colorBar=True, label='H2 Solution $u$',
-               showLater=True)
+ax, cbar = pg.show(gridh2, data=uh, colorBar=True, label='H2 Solution $u$',
+                   showLater=True)
 
 pg.mplviewer.drawMesh(ax, gridh2)
 
 """
-The same we do using a quadratic (P) refinement.
+.. image:: PLOT2RST.current_figure
+    :scale: 50
+
+We do the same using a quadratic (P) refinement, i.e. the same number of nodes.
 """
 
 gridp2 = grid.createP2()
 
 up = pg.solver.solve(gridp2, f=1.,
-                     uBoundary=[gridp2.findBoundaryByMarker(1,5), 0.0],
+                     uBoundary=[gridp2.findBoundaryByMarker(1, 5), 0.0],
                      verbose=True)
 
 """
@@ -136,7 +138,8 @@ uH2 = pg.interpolate(mesh=gridh2, data=uh, pos=probe)
 uP2 = pg.interpolate(mesh=gridp2, data=up, pos=probe)
 
 plt.figure()
-plt.plot(x, np.array(list(map(uAna, probe))), 'black', linewidth=2, label='analytical')
+plt.plot(x, np.array(list(map(uAna, probe))), 'black', linewidth=2,
+         label='analytical')
 plt.plot(x, uH1, label='linear (H1)')
 plt.plot(x, uH2, label='linear (H2)')
 plt.plot(x, uP2, label='quadratic (P2)')
