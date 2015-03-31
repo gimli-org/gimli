2d66a9f0487e6d85ea4fad90b9f917929a65ac85
halbmy <halbmy@gmail.com>
2015-03-31 17:07:45 +0000

    separated SIP code into several files (sip.py for the main class, plotting.py for plots, models.py for SIP relaxation models and importexport.py for reading/writing files) --> this could be done so for most methods helping to keep overview

---
+++ python/pygimli/physics/SIP/importexport.py
@@ -0,0 +1,125 @@
+from math import pi, log10, exp
+import numpy as np
+from scipy.integrate import simps
+import matplotlib.pyplot as plt
+import pygimli as pg
+
+
+def readTXTSpectrum(filename):
+    """ read spectrum from ZEL device output (txt) data file """
+    fid = open(filename)
+    lines = fid.readlines()
+    fid.close()
+    f, amp, phi = [], [], []
+    for line in lines[1:]:
+        snums = line.replace(';', ' ').split()
+        if len(snums) > 3:
+            f.append(float(snums[0]))
+            amp.append(float(snums[1]))
+            phi.append(-float(snums[3]))
+        else:
+            break
+
+    return np.asarray(f), np.asarray(amp), np.asarray(phi)
+
+
+def readSIP256file(resfile, verbose=False):
+    """
+    read SIP256 file (RES format) - mostly used for 2d SIP by pybert.sip
+
+    Parameters
+    ----------
+        filename - *.RES file (SIP256 raw output file)
+        verbose - do some output [False]
+
+    Returns
+    -------
+        header - dictionary of measuring setup
+        DATA - data AB-list of MN-list of matrices with f, Z, phi, dZ, dphi
+        AB - list of current injection
+        RU - list of remote units
+
+    Examples
+    --------
+        header, DATA, AB, RU = readSIP256file('myfile.res', True)
+    """
+    activeBlock = ''
+    header = {}
+    LINE = []
+    dataAct = False
+    with open(resfile, 'r') as f:
+        for line in f:
+            if dataAct:
+                LINE.append(line)
+            elif len(line):
+                if line[0] == '[':
+                    token = line[1:line.rfind(']')].replace(' ', '_')
+                    if token == 'Messdaten_SIP256':
+                        dataAct = True
+                    elif token[:3] == 'End':
+                        header[activeBlock] = np.array(header[activeBlock])
+                        activeBlock = ''
+                    elif token[:5] == 'Begin':
+                        activeBlock = token[6:]
+                        header[activeBlock] = []
+                    else:
+                        value = line[line.rfind(']') + 1:]
+                        try:  # direct line information
+                            if '.' in value:
+                                num = float(value)
+                            else:
+                                num = int(value)
+                            header[token] = num
+                        except Exception:  # maybe beginning or end of a block
+                            pass
+                else:
+                    if activeBlock:
+                        nums = np.array(line.split(), dtype=float)
+                        header[activeBlock].append(nums)
+
+    DATA, Data, data, AB, RU, ru = [], [], [], [], [], []
+    for line in LINE:
+        sline = line.split()
+        if line.find('Reading') == 0:
+            rdno = int(sline[1])
+            if rdno:
+                AB.append((int(sline[4]), int(sline[6])))
+            if ru:
+                RU.append(ru)
+                ru = []
+            if rdno > 1 and Data:
+                Data.append(np.array(data))
+                DATA.append(Data)
+                Data, data = [], []
+                if verbose:
+                    print('Reading ' + str(rdno - 1) + ':' + str(len(Data)) +
+                          ' RUs')
+        elif line.find('Remote Unit') == 0:
+            ru.append(int(sline[2]))
+            if data:
+                Data.append(np.array(data))
+                data = []
+        elif line.find('Freq') >= 0:
+            pass
+        elif len(sline) > 1 and rdno > 0:  # some data present
+            for c in range(6):
+                if len(sline[c]) > 11:  # too long line / missing space
+                    if c == 0:
+                        part1 = sline[c][:-15]
+                        part2 = sline[c][10:]
+                    else:
+                        part1 = sline[c][:-10]
+                        part2 = sline[c][9:]
+                    sline = sline[:c] + [part1] + [part2] + sline[c + 1:]
+            data.append(np.array(sline[:5], dtype=float))
+
+    Data.append(np.array(data))
+    DATA.append(Data)
+    if verbose:
+        print('Reading ' + str(rdno) + ':' + str(len(Data)) + ' RUs')
+
+    return header, DATA, AB, RU
+
+
+if __name__ == "__main__":
+    pass
---
+++ python/pygimli/physics/SIP/plotting.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+
+
+def showAmplitudeSpectrum(ax, freq, amp, ylabel=r'$\rho_a$ in $\Omega$m',
+                          grid=True, marker='+', ylog=True, **kwargs):
+    """ show amplitude spectrum """
+    ax.semilogx(freq, amp, marker=marker, label='obs', **kwargs)
+    if ylog:
+        ax.set_yscale('log')
+    ax.set_ylim(min(amp) * .99, max(amp * 1.01))
+    ax.set_xlabel('f in Hz')
+    ax.set_ylabel(ylabel)
+    ax.grid(grid)
+
+
+def showPhaseSpectrum(ax, freq, phi, ylabel=r'$\phi_a$ in mrad',
+                      grid=True, marker='+', ylog=False, **kwargs):
+    """ show phase spectrum """
+    ax.semilogx(freq, phi, marker=marker, label='obs', **kwargs)
+    if ylog:
+        ax.set_yscale('log')
+    ax.set_xlabel('f in Hz')
+    ax.set_ylabel(ylabel)
+    ax.grid(grid)
+
+
+def plotSpectrum(ax, freq, vals, ylabel=r'$\phi_a$ in mrad',
+                 grid=True, marker='+', ylog=True, **kwargs):
+    """ show phase spectrum """
+    ax.loglog(freq, vals, marker=marker, label='obs', **kwargs)
+    if ylog:
+        ax.set_yscale('log')
+    ax.set_xlabel('f in Hz')
+    ax.set_ylabel(ylabel)
+    ax.grid(grid)
+
+
+def showSpectrum(freq, amp, phi, nrows=2, ylog=None):
+    """ show amplitude and phase spectra in two subplots """
+    fig, ax = plt.subplots(nrows=nrows, sharex=(nrows == 2))
+    showAmplitudeSpectrum(ax[0], freq, amp, ylog=ylog)
+    showPhaseSpectrum(ax[1], freq, phi, ylog=ylog)
+    return fig, ax
+
+
+if __name__ == "__main__":
+    pass
