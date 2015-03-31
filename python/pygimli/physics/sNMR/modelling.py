d20a5d43eedb6022ddb15e5845df70028654ba1c
halbmy <halbmy@gmail.com>
2015-03-31 17:43:38 +0000

    separated MRS code into several files (mrs.py for the main class, plotting.py for plots, modelling.py for forward operators)

---
+++ python/pygimli/physics/sNMR/modelling.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Modelling classes for magnetic resonance sounding 
+"""
+
+# general modules to import according to standards
+import pygimli as pg
+import numpy as np
+
+
+class MRS1dBlockQTModelling(pg.ModellingBase):
+    """
+    MRS1dBlockQTModelling - pygimli modelling class for block-mono QT inversion
+
+    f=MRS1dBlockQTModelling(lay, KR, KI, zvec, t, verbose = False )
+    """
+    def __init__(self, nlay, K, zvec, t, verbose=False):
+        """ constructor with number of layers, kernel, z and t vectors """
+        mesh = pg.createMesh1DBlock(nlay, 2)  # thk, wc, T2*
+        pg.ModellingBase.__init__(self, mesh)
+        self.K_ = K
+        self.zv_ = np.array(zvec)
+        self.nl_ = nlay
+        self.nq_ = len(K)
+        self.t_ = np.array(t)
+        self.nt_ = len(t)
+
+    def response(self, par):
+        """ yield model response cube as vector """
+        nl = self.nl_
+        thk = par(0, nl - 1)
+        wc = par(nl - 1, 2 * nl - 1)
+        t2 = par(2 * nl - 1, 3 * nl - 1)
+        zthk = np.cumsum(thk)
+        zv = self.zv_
+        lzv = len(zv)
+        izvec = np.zeros(nl + 1, np.int32)
+        rzvec = np.zeros(nl + 1)
+        for i in range(nl - 1):
+            ii = (zv < zthk[i]).argmin()
+            izvec[i + 1] = ii
+            if ii <= len(zv):
+                rzvec[i + 1] = (zthk[i] - zv[ii - 1]) / (zv[ii] - zv[ii - 1])
+
+        izvec[-1] = lzv - 1
+        A = np.zeros((self.nq_, self.nt_), dtype=complex)
+        for i in range(nl):
+            wcvec = np.zeros(lzv - 1)
+            wcvec[izvec[i]:izvec[i + 1]] = wc[i]
+            if izvec[i + 1] < lzv:
+                wcvec[izvec[i + 1] - 1] = wc[i] * rzvec[i + 1]
+            if izvec[i] > 0:
+                wcvec[izvec[i] - 1] = wc[i] * (1 - rzvec[i])
+            amps = np.dot(self.K_, wcvec)
+            for ii, a in enumerate(A):
+                a += np.exp(-self.t_ / t2[i]) * amps[ii]
+
+        return pg.asvector(np.abs(A).ravel())
+
+if __name__ == "__main__":
+    pass
--- python/pygimli/physics/sNMR/mrs.py
+++ python/pygimli/physics/sNMR/mrs.py
@@ -1,167 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Was macht das Ding?
+    Magnetic resonance sounding module
 """
 
 # general modules to import according to standards
-import pygimli as pg
-import matplotlib.pyplot as plt
+import time
 import numpy as np
-
-# specific functions imported from pygimli
+import matplotlib.pyplot as plt
+from scipy.io import loadmat  # loading Matlab mat files
+from scipy.linalg import inv  # inverse matrix for model covariance
+# pygimli main package and specific functions
+import pygimli as pg
 from pygimli.utils import iterateBounds
 from pygimli.utils.base import gmat2numpy
-
-#from pygimli.viewer import drawModel1D
-from scipy.io import loadmat
-from scipy.linalg import inv
-import time
-
-
-# forward modelling class (physics)
-class MRS1dBlockQTModelling(pg.ModellingBase):
-
-    """
-    MRS1dBlockQTModelling - pygimli modelling class for block-mono QT inversion
-    f=MRS1dBlockQTModelling(lay, KR, KI, zvec, t, verbose = False )
-    """
-
-    def __init__(self, nlay, K, zvec, t, verbose=False):
-        """ constructor with number of layers, kernel, z and t vectors """
-        mesh = pg.createMesh1DBlock(nlay, 2)  # thk, wc, T2*
-        pg.ModellingBase.__init__(self, mesh)
-        self.K_ = K
-        self.zv_ = np.array(zvec)
-        self.nl_ = nlay
-        self.nq_ = len(K)
-        self.t_ = np.array(t)
-        self.nt_ = len(t)
-
-    def response(self, par):
-        """ yield model response cube as vector """
-        nl = self.nl_
-        thk = par(0, nl - 1)
-        wc = par(nl - 1, 2 * nl - 1)
-        t2 = par(2 * nl - 1, 3 * nl - 1)
-        zthk = np.cumsum(thk)
-        zv = self.zv_
-        lzv = len(zv)
-        izvec = np.zeros(nl + 1, np.int32)
-        rzvec = np.zeros(nl + 1)
-        for i in range(nl - 1):
-            ii = (zv < zthk[i]).argmin()
-            izvec[i + 1] = ii
-            if ii <= len(zv):
-                rzvec[i + 1] = (zthk[i] - zv[ii - 1]) / (zv[ii] - zv[ii - 1])
-
-        izvec[-1] = lzv - 1
-        A = np.zeros((self.nq_, self.nt_), dtype=complex)
-        for i in range(nl):
-            wcvec = np.zeros(lzv - 1)
-            wcvec[izvec[i]:izvec[i + 1]] = wc[i]
-            if izvec[i + 1] < lzv:
-                wcvec[izvec[i + 1] - 1] = wc[i] * rzvec[i + 1]
-            if izvec[i] > 0:
-                wcvec[izvec[i] - 1] = wc[i] * (1 - rzvec[i])
-            amps = np.dot(self.K_, wcvec)
-            for ii, a in enumerate(A):
-                a += np.exp(-self.t_ / t2[i]) * amps[ii]
-
-        return pg.asvector(np.abs(A).ravel())
-
-# plotting functions (just a copy for now, later from pygimli.mplviewer)
-
-
-def drawModel1D(ax, thickness, values, plotfunction='plot',
-                xlabel='', *args, **kwargs):
-    """Draw 1d block model into axis ax defined by values and thickness vectors
-    using plotfunction."""
-
-    nLayers = len(thickness) + 1
-    px = np.zeros(nLayers * 2)
-    pz = np.zeros(nLayers * 2)
-    z1 = np.cumsum(thickness)
-
-    for i in range(nLayers):
-        px[2 * i] = values[i]
-        px[2 * i + 1] = values[i]
-
-        if i == nLayers - 1:
-            pz[2 * i + 1] = z1[i - 1] * 1.2
-            pass
-        else:
-            pz[2 * i + 1] = z1[i]
-            pz[2 * i + 2] = z1[i]
-
-    if plotfunction == 'loglog' or plotfunction == 'semilogy':
-        pz[0] = thickness[0] * 0.8
-
-    try:
-        plot = getattr(ax, plotfunction)
-        plot(px, pz, *args, **kwargs)
-    except Exception as e:
-        print(e)
-
-    ax.set_ylabel('Depth [m]')
-    ax.set_xlabel(xlabel)
-    ax.set_ylim(pz[-1], pz[0])
-    ax.grid(True)
-
-
-def showErrorBars(ax, thk, val, thkL, thkU, valL, valU, *args, **kwargs):
-    """ plot error bars into a plot """
-    zb = np.cumsum(thk)
-    zm = np.hstack((zb - thk / 2, zb[-1] * 1.2))  # zb[-1]+thk[-1]/2))
-    valm = (val[:-1] + val[1:]) / 2
-    xerr = [val - valL, valU - val]
-    yerr = [thk - thkL, thkU - thk]
-    ax.errorbar(val, zm, fmt='.', xerr=xerr, ecolor='r', **kwargs)
-    ax.errorbar(valm, zb, fmt='.', yerr=yerr, ecolor='g', **kwargs)
-    ax.set_ylim(bottom=zm[-1] * 1.02, top=0)
-
-
-def showWC(ax, thk, wc, wmin=0., wmax=0.45, maxdep=0., dw=0.05, **kwargs):
-    """ show water content function nicely """
-    drawModel1D(ax, thk, wc, xlabel=r'$\theta$')
-    ax.set_xlim(0., 0.45)
-    if maxdep > 0.:
-        ax.set_ylim(maxdep, 0.)
-    wt = np.arange(wmin, wmax, dw)
-    ax.set_xticks(wt)
-    ax.set_xticklabels([str(wi) for wi in wt])
-
-
-def showT2(ax, thk, t2, maxdep=0., **kwargs):
-    """ show T2 function nicely """
-    drawModel1D(
-        ax,
-        thk,
-        t2 * 1e3,
-        xlabel=r'$T_2^*$ [ms]',
-        plotfunction='semilogx')
-    tmin = min(20, min(t2) * 0.9e3)
-    tmax = max(500, max(t2) * 1.1e3)
-    ax.set_xlim(tmin, tmax)
-    if maxdep > 0.:
-        ax.set_ylim(maxdep, 0.)
-    xt = [20, 50, 100, 200, 500]
-    ax.set_xticks(xt)
-    ax.set_xticklabels([str(ai) for ai in xt])
-
-# Data handling class (logics)
+# local functions in package
+from modelling import MRS1dBlockQTModelling
+from plotting import showErrorBars, showWC, showT2, drawModel1D
 
 
 class MRS():
-
     """
-    class for managing a magnetic resonance sounding (MRS)
+    magnetic resonance sounding (MRS) manager class
     """
 
-# def
-# __init__(self,name=None,defaultNoise=150e-9,usereal=False,verbose=True):
-# # constructor with dummy values
-    # constructor with dummy values
     def __init__(self, name=None, verbose=True, **kwargs):
         """ init function with optional data load from mrsi file """
         self.verbose = verbose
@@ -173,10 +35,12 @@ class MRS():
         self.upperBound = [30., 0.45, 1.00]  # d, theta, T2*
         self.startval = [10., 0.30, 0.20]  # d, theta, T2*
         self.logpar = False
+        self.basename = 'new'
         if name is not None:  # load data and kernel
             # check for mrsi/d/k
             if name[-5:].lower() == '.mrsi':
                 self.loadMRSI(name, **kwargs)
+                self.basename = datafile.rstrip('.mrsi')
             else:  # else mrsd+k?
                 self.loadDir(name)
 
@@ -189,12 +53,11 @@ class MRS():
             out += ", %d layers" % len(self.z)
         return out + ">"
 
-    def loadMRSI(
-            self, filename, defaultNoise=100e-9, usereal=False, mint=0., maxt=2.0):
+    def loadMRSI(self, filename, defaultNoise=100e-9, usereal=False,
+                 mint=0., maxt=2.0):
         """ load data, error and kernel from mrsi file """
         idata = loadmat(filename, struct_as_record=False,
                         squeeze_me=True)['idata']
-        #self.t = idata.data.t + idata.data.effDead
         ttmp = idata.data.t + idata.data.effDead
         good = (ttmp <= maxt) & (ttmp >= mint)
         self.t = ttmp[good]
@@ -391,7 +254,7 @@ class MRS():
         """ return block model results """
         return self.splitModel()
 
-    def showResult(self, figsize=(10, 8), save='', show=False):
+    def showResult(self, figsize=(10, 8), save=''):
         """ show theta(z) and T2*(z) (+uncertainties if there) """
         fig, ax = plt.subplots(1, 2, sharey=True, figsize=figsize)
         thk, wc, t2 = self.splitModel()
@@ -401,19 +264,10 @@ class MRS():
             thkL, wcL, t2L = self.splitModel(self.modelL)
             thkU, wcU, t2U = self.splitModel(self.modelU)
             showErrorBars(ax[0], thk, wc, thkL, thkU, wcL, wcU)
-            showErrorBars(
-                ax[1],
-                thk,
-                t2 * 1e3,
-                thkL,
-                thkU,
-                t2L * 1e3,
-                t2U * 1e3)
+            showErrorBars(ax[1], thk, t2*1e3, thkL, thkU, t2L*1e3, t2U*1e3)
 
         if save:
             fig.savefig(save, bbox_inches='tight')
-        if show:
-            plt.show()
         return fig, ax
 
     def showResultAndFit(self, figsize=(12, 10), save='', plotmisfit=False,
@@ -431,10 +285,7 @@ class MRS():
             thkL, wcL, t2L = self.splitModel(self.modelL)
             thkU, wcU, t2U = self.splitModel(self.modelU)
             showErrorBars(ax[0, 0], thk, wc, thkL, thkU, wcL, wcU)
-            showErrorBars(ax[0, 1], thk, t2 *
-                          1e3, thkL, thkU, t2L *
-                          1e3, t2U *
-                          1e3)
+            showErrorBars(ax[0, 1], thk, t2*1e3, thkL, thkU, t2L*1e3, t2U*1e3)
 
         if maxdep > 0.:
             ax[0, 0].set_ylim([maxdep, 0.])
@@ -445,17 +296,15 @@ class MRS():
             ax[1, 1], self.INV.response() * 1e9, clim=clim, islog=False)
         ax[1, 1].set_title('simulated data [nV]')  # log10
         if plotmisfit:
-            self.showCube(
-                ax[0, 2], (self.data - self.INV.response()) * 1e9, islog=False)
+            self.showCube(ax[0, 2], (self.data - self.INV.response()) * 1e9,
+                          islog=False)
             ax[0, 2].set_title('misfit [nV]')  # log10
-            self.showCube(
-                ax[1, 2], (self.data - self.INV.response()) / self.error, islog=False)
+            ewmisfit = (self.data - self.INV.response()) / self.error
+            self.showCube(ax[1, 2], ewmisfit, islog=False)
             ax[1, 2].set_title('error-weighted misfit')
 
         if save:
             fig.savefig(save, bbox_inches='tight')
-        if show:
-            plt.show()
         return fig, ax
 
     def saveResult(self, filename):
@@ -527,18 +376,22 @@ class MRS():
 
         @inspyred.ec.evaluators.evaluator
         def datafit(individual, args):
-            misfit = (
-                self.data - self.f.response(self.genMod(individual))) / self.error
+            misfit = (self.data - self.f.response(self.genMod(individual))) / \
+                self.error
             return np.mean(misfit**2)
 
         # prepare forward operator
         if self.f is None or (nlay is not None and nlay is not self.nlay):
             self.createFOP(nlay)
 
-        lowerBound = pg.cat(pg.cat(pg.RVector(self.nlay - 1, self.lowerBound[0]),
-                                   pg.RVector(self.nlay, self.lowerBound[1])), pg.RVector(self.nlay, self.lowerBound[2]))
-        upperBound = pg.cat(pg.cat(pg.RVector(self.nlay - 1, self.upperBound[0]),
-                                   pg.RVector(self.nlay, self.upperBound[1])), pg.RVector(self.nlay, self.upperBound[2]))
+        lowerBound = pg.cat(pg.cat(pg.RVector(self.nlay - 1,
+                                              self.lowerBound[0]),
+                                   pg.RVector(self.nlay, self.lowerBound[1])),
+                            pg.RVector(self.nlay, self.lowerBound[2]))
+        upperBound = pg.cat(pg.cat(pg.RVector(self.nlay - 1,
+                                              self.upperBound[0]),
+                                   pg.RVector(self.nlay, self.upperBound[1])),
+                            pg.RVector(self.nlay, self.upperBound[2]))
         if self.logpar:
             self.lLB, self.lUB = pg.log(lowerBound), pg.log(
                 upperBound)  # ready mapping functions
@@ -572,12 +425,13 @@ class MRS():
         else:
             ea.terminator = inspyred.ec.terminators.evaluation_termination
 
-        #ea.observer = my_observer
+#        ea.observer = my_observer
         ea.observer = [
             inspyred.ec.observers.stats_observer,
             inspyred.ec.observers.file_observer]
-        self.pop = ea.evolve(evaluator=datafit, generator=mygenerate, maximize=False,
-                             pop_size=pop_size, max_evaluations=max_evaluations, num_elites=1,
+        self.pop = ea.evolve(evaluator=datafit, generator=mygenerate,
+                             maximize=False, pop_size=pop_size,
+                             max_evaluations=max_evaluations, num_elites=1,
                              bounder=inspyred.ec.Bounder(0., 1.), **kwargs)
         self.pop.sort(reverse=True)
         self.fits = [ind.fitness for ind in self.pop]
@@ -602,8 +456,8 @@ class MRS():
         thk = model[:self.nlay - 1]
         wc = model[self.nlay - 1:self.nlay * 2 - 1]
         t2 = model[self.nlay * 2 - 1:]
-        drawModel1D(ax[0], thk, wc * 100, color='black', linewidth='5')
-        drawModel1D(ax[1], thk, t2 * 1000, color='black', linewidth='5')
+        drawModel1D(ax[0], thk, wc * 100, color='black', linewidth=5)
+        drawModel1D(ax[1], thk, t2 * 1000, color='black', linewidth=5)
 
         ax[0].set_xlim(self.lowerBound[1] * 100, self.upperBound[1] * 100)
         ax[0].set_ylim((maxz * 1.2, 0))
@@ -616,37 +470,11 @@ class MRS():
         plt.show()
 
 if __name__ == "__main__":
-    import sys  # not really PEP-7 conform to import here
-    from optparse import OptionParser
-
-    parser = OptionParser("usage: %prog [options] mrs",
-                          version="%prog: " + pg.__version__)
-    parser.add_option(
-        "-v",
-        "--verbose",
-        dest="verbose",
-        action="store_true",
-        help="be verbose",
-        default=False)
-    parser.add_option("-n", "--nLayers", dest="nlay",
-                            help="number of layers", type="int",
-                            default="4")
-
-    (options, args) = parser.parse_args()
-
-    if options.verbose:
-        __verbose__ = True
-
-    if len(args) == 0:
-        parser.print_help()
-        print("Please add a mesh or model name.")
-        sys.exit(2)
-    else:
-        datafile = args[0]
-
+    datafile = 'example.mrsi'
+    nlay = 4
     mrs = MRS(datafile)
-    mrs.run(options.nlay, uncertainty=True)
+    mrs.run(nlay, uncertainty=True)
     thk, wc, t2 = mrs.result()
-    name = datafile.rstrip('.mrsi')
-    mrs.saveResult(name + '.result')
-    mrs.showResultAndFit(save=name + '.pdf', show=True)
+    mrs.saveResult(mrs.basename+'.result')
+    mrs.showResultAndFit(save=mrs.basename+'.pdf')
+    plt.show()
