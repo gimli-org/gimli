7ae67a89abdfcaf8bf5f47981e68005e05889d7b
florian-wagner <wagnerfl@student.ethz.ch>
2015-04-03 18:32:47 +0000

    added setup.py, fixed cmake 3.0 issue

---
+++ python/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Setup script for pygimli and python apps.
+
+Usage:
+
+    python setup.py install --user # for local installation
+    sudo python setup.py install # for system-wide installation
+
+"""
+
+import os
+from setuptools import setup, find_packages
+import pygimli as pg
+
+apps = ["apps/" + app for app in os.listdir('apps') if not "template" in app]
+
+with open(os.path.join("../README.rst"), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name="pygimli",
+      version=pg.__version__,
+      description="Geophysical Inversion and Modelling Library",
+      long_description=long_description,
+      author="Carsten Rücker, Thomas Günther, Florian Wagner",
+      author_email="mail@pygimli.org",
+      license="LPGL",
+      url="http://www.pygimli.org",
+      packages=find_packages(),
+      package_data={'': ['*.so']},
+      scripts=apps)
