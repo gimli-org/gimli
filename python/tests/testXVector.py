2e42102ed9366a8c1bee1b1d136bd6861898abaa
carsten-forty2 <carsten.ruecker@tu-berlin.de>
2015-07-01 16:00:25 +0000

    add RVector +/-/*// int operators for castxml build

--- python/tests/testXVector.py
+++ python/tests/testXVector.py
@@ -27,6 +27,9 @@ class TestRVectorMethods(unittest.TestCase):
 
     def test_RVectorOP(self):
         v = pg.RVector(5, 1.0)
+
+        print(v - 2)
+
         
         self.assertEqual(sum(v + 1), 10)
         self.assertEqual(sum(v - 2), -5)
