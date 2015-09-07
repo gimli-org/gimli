a696d3e1d55a3a09156cdd842f9f069d4fb7f24c
halbmy <halbmy@gmail.com>
2015-09-07 18:30:02 +0000

    add string switch to setParameters minor corrections (mostly orthography)

--- src/modellingbase.h
+++ src/modellingbase.h
@@ -26,8 +26,8 @@
 //#include "blockmatrix.h"
 
 namespace GIMLI{
-    
-//class H2SparseMapMatrix;    
+
+//class H2SparseMapMatrix;
 
 /*! General modelling interface class.*/
 class DLLEXPORT ModellingBase{
@@ -55,25 +55,25 @@ public:
 
     /*! Change the associated data container */
     void setData(DataContainer & data);
-    
+
     /*! Return the associated data container. */
     DataContainer & data() const;
-    
-    /*! Interface to define custom start model generators in you forward 
+
+    /*! Interface to define custom start model generators in you forward
      * operators.
      * !*/
     virtual RVector createDefaultStartModel() { return RVector(0); }
 
-    /*! Return the start model. 
-     * If there is no model defined createDefaultStartModel is called which can 
+    /*! Return the start model.
+     * If there is no model defined createDefaultStartModel is called which can
      * be overloaded by child classes.
      !*/
     virtual RVector startModel();
 
-    virtual void setStartModel(const RVector & startModel){ 
+    virtual void setStartModel(const RVector & startModel){
         startModel_=startModel; }
 
-    /*! Set new mesh to the forward operator, optionally hold region parameter 
+    /*! Set new mesh to the forward operator, optionally hold region parameter
      * for the new mesh (i.e. for roll a long) */
     void setMesh(const Mesh & mesh, bool holdRegionInfos=false);
 
@@ -81,87 +81,89 @@ public:
 
     /*! Set external Jacobian matrix*/
     virtual void setJacobian(MatrixBase * J);
-    
+
     /*! Create and fill the Jacobian matrix with a given model vector. */
     virtual void createJacobian(const RVector & model);
-    
+
     /*! Here you should initialize your Jacobian matrix. Default is RMatrix()*/
     virtual void initJacobian();
-    
+
     /*! Return the pointer to the Jacobian matrix associated with this forward operator. */
     MatrixBase * jacobian() { return jacobian_; }
-    
+
     /*! Return the pointer to the Jacobian matrix associated with this forward operator. */
     MatrixBase * jacobian() const { return jacobian_; }
-    
-    /*! Return the Jacobian Matrix (read only) associated with this forward operator. 
+
+    /*! Return the Jacobian Matrix (read only) associated with this forward operator.
      *  Throws an exception if the jacobian is not initialized. Cannot yet be overloaded py pyplusplus (return virtual reference)(Warning 1049). */
-    virtual RMatrix & jacobianRef() const { 
+    virtual RMatrix & jacobianRef() const {
         if (! jacobian_) {
             throwError(1, WHERE_AM_I + " Jacobian matrix is not initialized.");
         }
-        return *dynamic_cast< RMatrix * >(jacobian_); 
+        return *dynamic_cast< RMatrix * >(jacobian_);
     }
-    
-    virtual RMatrix & jacobianRef() { 
+
+    virtual RMatrix & jacobianRef() {
         if (! jacobian_) {
             throwError(1, WHERE_AM_I + " Jacobian matrix is not initialized.");
         }
-        return *dynamic_cast< RMatrix * >(jacobian_); 
+        return *dynamic_cast< RMatrix * >(jacobian_);
     }
-    
+
     /*! Clear Jacobian matrix. */
     virtual void clearJacobian(){ jacobian_->clear(); }
-    
+
     /*! Set and get constraint matrix */
 //     inline void setConstraintMatrix(const RSparseMapMatrix & C){ C_ = C; }
 //     inline const RSparseMapMatrix & constraintMatrix() const { return C_; }
 //         /*! Clear Constraints matrix */
     virtual void setConstraints(MatrixBase * C);
-    
+
     virtual void clearConstraints();
 
     virtual void initConstraints();
-    
+
     virtual void createConstraints();
-    
+
     virtual MatrixBase * constraints();
-    
+
     virtual MatrixBase * constraints() const;
-    
+
     virtual RSparseMapMatrix & constraintsRef() const;
 
     virtual RSparseMapMatrix & constraintsRef();
-    
+
     const RMatrix & solution() const { return solutions_; }
 
     void createRefinedForwardMesh(bool refine=true, bool pRefine=false);
-    
+
     /*! Set refined mesh for forward calculation. */
     void setRefinedMesh(const Mesh & mesh);
 
     void mapModel(const RVector & model, double background=0);
-   
+
     const RegionManager & regionManager() const;
 
     RegionManager & regionManager();
+
+    bool verbose() { return verbose_; }
 
     Region * region(int marker);
 
     RVector createStartVector();
 
     void initRegionManager();
-     
+
     /*! Set the maximum number of allowed threads for MT calculation. Have to be greater than 0 */
     void setThreadCount(uint nThreads) { nThreads_=max(1, (int)nThreads); }
-    
+
     /*! Return the maximum number of allowed threads for MT calculation */
     uint threadCount() const { return nThreads_; }
 
 protected:
-    
+
     virtual void init_();
-    
+
     virtual void deleteMeshDependency_(){}
 
     virtual void updateMeshDependency_(){}
@@ -169,27 +171,27 @@ protected:
     virtual void updateDataDependency_(){}
 
     void setMesh_(const Mesh & mesh);
-    
+
     Mesh                    * mesh_;
 
     DataContainer           * dataContainer_;
-    
+
     MatrixBase              * jacobian_;
     bool                    ownJacobian_;
-    
+
     MatrixBase              * constraints_;
     bool                    ownConstraints_;
-    
+
     RMatrix                 solutions_;
 
     RVector                 startModel_;
-    
+
     bool                    verbose_;
-    
+
     bool                    regionManagerInUse_;
-    
+
     uint nThreads_;
-    
+
 private:
     RegionManager            * regionManager_;
 
