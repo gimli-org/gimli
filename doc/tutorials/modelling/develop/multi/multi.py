6ab0ec13a2ce8a3ddb70e51fc95ff91229fa426c
carsten-forty2 <carsten@gimli.org>
2015-11-13 16:18:02 +0100

    add multiproc to multi

--- doc/tutorials/modelling/develop/multi/multi.py
+++ doc/tutorials/modelling/develop/multi/multi.py
@@ -8,6 +8,8 @@ from multi_darcy_flow import darcyFlow
 from multi_advection import calcSaturation
 from multi_ert import simulateERTData
 
+from multiprocessing import Process, Queue
+
 
 class WorkSpace():
     pass
@@ -18,12 +20,53 @@ class MulitFOP(pg.ModellingBase):
 
         self.tSteps = tSteps
         self.regionManager().setParameterCount(3)
-        self.setStartModel([1e-7, 0.01, 0.0001])
+        self.setStartModel([1e-7, 0.01, 0.001])
             
         self.ws = WorkSpace()
         self.timesAdvection = np.linspace(1, tMax, tSteps)
         self.peclet = peclet
+        self._J = pg.RMatrix()
+        self.setJacobian(self._J)       
+        
+        
+    def createJacobian(self, model):
+        nModel = len(model)
+        resp = self.response(model)
+        nData = len(resp)
+        
+        self._J.resize(nData, nModel)
+        self._J *= 0.
+        
+        respChange = pg.RMatrix(nModel, nData)
+        dModel = pg.RVector(len(model))
+
+        fak = 1.05
+        q = Queue()
+         
+        for i in range(nModel):
+            p = Process(target=self.createResponseProc, args=(model, fak, i, q))
+            p.start()
+        p.join()
             
+        for i in range(nModel):
+            Imodel, respChange, dModel = q.get()
+            dData = respChange - resp
+            self._J.setCol(Imodel, dData/dModel)
+    
+    def createResponseProc(self, model, fak, i, q):
+        print("proc:", i)
+        modelChange = pg.RVector(model)
+        modelChange[i] *= fak
+                
+        mesh, vel, p, k = darcyFlow(model=model[0:3], verbose=0)
+        sat = calcSaturation(mesh, vel.T, self.timesAdvection, 
+                             injectPos=[2., -2.], peclet=self.peclet, verbose=0)
+        sampleTime = [0, self.tSteps/2, self.tSteps-1]
+        meshERT, dataERT, res, rhoa, err = simulateERTData(sat[sampleTime], mesh, verbose=0)
+
+        q.put([i, rhoa.flatten(), modelChange[i]-model[i]])
+        #return rhoa.flatten(), modelChange[i]-model[i]
+                
     def response(self, par):
         ws = self.ws
         print('create response for: ' + str(par))
@@ -39,10 +82,9 @@ class MulitFOP(pg.ModellingBase):
         
         print(".. dc step")
         sampleTime = [0, self.tSteps/2, self.tSteps-1]
-        ws.meshERT, ws.dataERT, ws.res, ws.rhoa, ws.err = simulateERTData(ws.saturation[sampleTime], ws.mesh, verbose=1)
+        ws.meshERT, ws.dataERT, ws.res, ws.rhoa, ws.err = simulateERTData(ws.saturation[sampleTime], ws.mesh, verbose=0)
 
         return ws.rhoa.flatten()
-    
     
     
 def test(model, tMax=5000, tSteps=40, peclet=50000):
@@ -65,6 +107,7 @@ def test(model, tMax=5000, tSteps=40, peclet=50000):
     pg.show(ws.meshERT, fop.ws.res[-1], label='resistivity in Ohm m')
 
     pg.wait()
+    
 if __name__ == '__main__':
     
     #test(model=[1e-7, 0.01, 0.0001], tMax=5000, tSteps=40, peclet=500000)
@@ -72,7 +115,7 @@ if __name__ == '__main__':
     fop = MulitFOP(verbose=1, tMax=5000, tSteps=40, peclet=500000)
     ert = fop.response([1e-7, 0.01, 0.001])
 
-    err = fop.ws.ertError.flatten()
+    err = fop.ws.err.flatten()
         
     inv = pg.RInversion(ert, fop, verbose=1, dosave=1)
         
