859465699e6168b0d69ef0020d03563b618f4320
carsten-forty2 <carsten@gimli.org>
2015-12-01 14:49:22 +0100

    try to adapt for different linux distros

--- scripts/install/install_linux_gimli.sh
+++ scripts/install/install_linux_gimli.sh
@@ -5,34 +5,56 @@ PARALELL=2
 
 CMAKE_GENERATOR='Unix Makefiles'
 
-buildGIMLI(){
-        if [ -d "gimli" ]; then
-            pushd gimli
-                git pull   
-            popd  
-        else
-            git clone https://github.com/gimli-org/gimli.git
-        fi
-          
-        [ -n "$BRANCH" ] && git checkout $BRANCH  
-        
-        chmod +x gimli/python/apps/*
-        
-        rm -rf build/
-        mkdir -p build
-    pushd build
-            cmake -G "$CMAKE_GENERATOR" ../gimli 
+case "$(grep "ID=" /etc/os-release)" in
+    *"gentoo"*)
+        echo "Gentoo system found"
+        PYTHONSPECS=''
+    ;;
+    *"debian"*)
+        echo "Debian system found"
+        PYTHONSPECS='-DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.4m.so.1.0
+                    -DBoost_PYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libboost_python-py34.so 
+                    -DPYTHON_EXECUTABLE=/usr/bin/python3'
+    ;;
+    *"arch"*)
+        echo "Arch Linux system found"
+        PYTHONSPECS='-DBoost_PYTHON_LIBRARY=/usr/lib64/libboost_python3.so'
+    ;;
+    *) 
+        echo $(grep "ID=" /etc/os-release) "system found: trying defaults"
+        PYTHONSPECS=''
+    ;;
+esac
 
-            make -j$PARALELL && make pygimli J=$PARALELL 
-            echo ""
-            echo ""
-            echo "============================================================================"
-            echo "---  try some basic test: calling pygimli once------------------------------"
-            export PYTHONPATH=$PYTHONPATH:$ROOT/gimli/python
-            python -c 'import pygimli as pg; print("pygimli version:", pg.__version__)'
-            echo "--- ------------------------------------------------------------------------"
-            echo "export PYTHONPATH=\$PYTHONPATH:$ROOT/gimli/python" > $ROOT/.bash_hint_pygimli
-            echo "export PATH=\$PATH:$ROOT/gimli/python/apps" >> $ROOT/.bash_hint_pygimli
+buildGIMLI(){
+    if [ -d "gimli" ]; then
+        pushd gimli
+            git pull   
+        popd  
+    else
+        git clone https://github.com/gimli-org/gimli.git
+    fi
+      
+    [ -n "$BRANCH" ] && git checkout $BRANCH  
+    
+    chmod +x gimli/python/apps/*
+    
+    rm -rf build/
+    mkdir -p build
+
+    pushd build
+        cmake -G "$CMAKE_GENERATOR" ../gimli $PYTHONSPECS
+
+        make -j$PARALELL && make pygimli J=$PARALELL 
+        echo ""
+        echo ""
+        echo "============================================================================"
+        echo "---  try some basic test: calling pygimli once------------------------------"
+        export PYTHONPATH=$PYTHONPATH:$ROOT/gimli/python
+        python -c 'import pygimli as pg; print("pygimli version:", pg.__version__)'
+        echo "--- ------------------------------------------------------------------------"
+        echo "export PYTHONPATH=\$PYTHONPATH:$ROOT/gimli/python" > $ROOT/.bash_hint_pygimli
+        echo "export PATH=\$PATH:$ROOT/gimli/python/apps" >> $ROOT/.bash_hint_pygimli
            
     popd
 }
