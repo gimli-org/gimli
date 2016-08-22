442a77b6f49206c0ec2d8fa3c760fd88076091f9
halbmy <thomas@pygimli.org>
2016-08-22 20:32:01 +0200

    improved docstrings for SIPspectrum class and clean up import a bit

--- python/pygimli/physics/SIP/__init__.py
+++ python/pygimli/physics/SIP/__init__.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    What we have here?
+    Spectral induced polarization (SIP) measurements and fittings 
 """
 
 from .sipspectrum import SIPSpectrum
-from .models import ColeCole
-from .importexport import readSIP256file, fstring
-# from . tools import *
-# from . plotting import *
 
 #__all__ = [SIPSpectrum, ColeCole, readSIP256file, fstring]
