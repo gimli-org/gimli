15f9bb5e2daae5696e2646c1ae35478751b52aad
halbmy <thomas@pygimli.org>
2016-09-09 08:36:45 +0200

    add Cm05 matrix type for covariance based inversion (e.g. using geostatistical regularization)

--- python/pygimli/core/matrix.py
+++ python/pygimli/core/matrix.py
@@ -4,7 +4,10 @@
 Some matrix specialization.
 """
 
+import time
 from pygimli.core import _pygimli_ as pg
+import numpy as np
+from scipy.linalg import eigh, get_blas_funcs
 
 
 class LMultRMatrix(pg.MatrixBase):
@@ -94,3 +97,40 @@ class LRMultRMatrix(pg.MatrixBase):
     def transMult(self, x):
         """ multiplication from right-hand-side (dot product) """
         return self.A.transMult(x * self.left) * self.right
+
+
+class Cm05Matrix(pg.MatrixBase):
+    """ matrix consisting of actual RMatrix and lef-side vector"""
+    def __init__(self, A, verbose=False):
+        """ constructor saving matrix and vector """
+        if A.shape[0] != A.shape[1]:  # rows/cols for pg matrix
+            raise Exception("Matrix must by square (and symmetric)!")
+
+        self.size = A.shape[0]
+        t = time.time()
+        self.ew, self.EV = eigh(A)
+        self.mul = np.sqrt(1./self.ew)
+        elapsed = time.time() - t
+        print('(C) Calculation time for eigenvalue decomposition:\n%s sec'
+              % elapsed)
+
+        self.A = A
+        super().__init__(verbose)  # only in Python 3
+#        pg.MatrixBase.__init__(self)  # the Python 2 variant
+
+    def rows(self):
+        """ return number of rows (using underlying matrix) """
+        return self.size
+
+    def cols(self):
+        """ return number of columns (using underlying matrix) """
+        return self.size
+
+    def mult(self, x):
+        """ multiplication from right-hand-side (dot product) """
+        return self.EV.dot((np.dot(np.transpose(x), self.EV)*self.mul).T)
+#        return self.EV.dot((x.T.dot(self.EV)*self.mul).T)
+
+    def transMult(self, x):
+        """ multiplication from right-hand-side (dot product) """
+        return self.mult(x)
