830b712458095d947ca04bb2e40da6471bceee7b
carsten-forty2 <carsten@gimli.org>
2017-05-18 17:02:56 +0200

    try to improve py-version selection

--- python/pygimli/physics/SIP/__init__.py
+++ python/pygimli/physics/SIP/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-    Spectral induced polarization (SIP) measurements and fittings 
+    Spectral induced polarization (SIP) measurements and fittings
 """
+
+from .models import ColeColePhi
 
 from .sipspectrum import SIPSpectrum
 
