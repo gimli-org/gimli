27a5c95d8949cd226f4b2a7daba0c71a8a722df7
Thomas Günther <thomas@pygimli.org>
2017-08-02 18:40:06 +0200

    a bit of pep

--- python/pygimli/physics/em/tdem.py
+++ python/pygimli/physics/em/tdem.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Time Domain Electromagnetics (TDEM) functions and class
-"""
+"""Time Domain Electromagnetics (TDEM) functions and class"""
 import sys
 from math import pi
 import numpy as np
