77bf150f8c1f38ed6bc1f42908db866941d37431
Florian Wagner <mail@fwagner.info>
2018-10-12 13:01:06 -0700

    Move mesh related monkeypatching to core/mesh.py and add createSecondaryNodes method

--- python/pygimli/testing/test_Traveltime.py
+++ python/pygimli/testing/test_Traveltime.py
@@ -18,31 +18,13 @@ mesh = pg.createGrid([0,1,2],[0,1,2])
 # Slowness
 slo = [1,2,1,4]
 
-
-# With secondary nodes
-def createMeshWithSecNodes(mesh, n=1):
-    secMesh = pg.Mesh(mesh)
-    secMesh.createNeighbourInfos()
-    for b in secMesh.boundaries():
-        A = b.node(0).pos()
-        B = b.node(1).pos()
-        line = pg.Line(A, B)
-        for i in range(n):
-            secNode = secMesh.createNode(line.at((i + 1) / (n + 1)))
-            if b.rightCell() is not None:
-                b.rightCell().addSecondaryNode(secNode)
-            if b.leftCell() is not None:
-                b.leftCell().addSecondaryNode(secNode)
-    return secMesh
-
 def test_withoutSecNodes():
     fop = pg.TravelTimeDijkstraModelling(mesh, data)
     t_normal = fop.response(slo)
-
     np.testing.assert_allclose(t_normal, 1 + np.sqrt(2))
 
 def test_withSecNodes():
-    mesh2 = createMeshWithSecNodes(mesh, n=3)
+    mesh2 = mesh.createSecondaryNodes(n=3)
     fop = pg.TravelTimeDijkstraModelling(mesh2, data)
     t_refined = fop.response(slo)
     np.testing.assert_allclose(t_refined, np.sqrt(5)) # only works if n_secNodes is odd number
