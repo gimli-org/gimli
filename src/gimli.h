b03df31f4eb2796b18eaabfd2237887a33e9df21
carsten-forty2 <carsten@pygimli.org>
2018-10-17 13:36:30 +0200

    separate nodes and secnodes

--- src/gimli.h
+++ src/gimli.h
@@ -148,7 +148,7 @@ typedef int64_t int64;
 
 #define ASSERT_EQUAL_SIZE(m, n) if (m.size() != n.size()) \
     throwLengthError(1, WHERE_AM_I + " " + str(m.size()) + " != " + str(n.size()));
-#define ASSERT_SIZE(vec, n) if (n >= vec.size()) \
+#define ASSERT_SIZE(vec, n) if (n < 0 || n >= vec.size()) \
     throwLengthError(1, WHERE_AM_I + " " + str(vec.size()) + " <= " + str(n));
 #define ASSERT_EQUAL(m, n) if (m != n) \
     throwLengthError(1, WHERE_AM_I + " " + str(m) + " != " + str(n));
