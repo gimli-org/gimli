aeb2febe9e83858020a4fc44e6a49a398b8aa1a2
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-12-09 21:57:14 +0100

    - add parameter **markerPosition** to createRectangle - add parameter **markerPosition** to createPolygon - minor wording changes

--- python/pygimli/meshtools/polytools.py
+++ python/pygimli/meshtools/polytools.py
@@ -61,12 +61,16 @@ def createRectangle(start=None, end=None, pos=None, size=None, **kwargs):
     pos : [x, y]
         Center position. The rectangle will be moved.
     size : [x, y]
-        width and height. The rectangle will be scaled.
+        Factors for x and y by which the rectangle, defined by **start** and
+        **width**, are scaled.
 
     **kwargs:
 
         marker : int [1]
             Marker for the resulting triangle cells after mesh generation
+        markerPosition : floats [x, y] [pos + (end - start) * 0.2]
+            Absolute position of the marker (works for both regions and
+            holes).
         area : float [0]
             Maximum cell size for resulting triangles after mesh generation
         boundaryMarker : int [1]
@@ -91,6 +95,13 @@ def createRectangle(start=None, end=None, pos=None, size=None, **kwargs):
     >>> rectangle = createRectangle(start=[4, -4], end=[6, -6],
     ...                             marker=4, area=0.1)
     >>> _ = pg.show(rectangle)
+
+    >>> import matplotlib.pyplot as plt
+    >>> import pygimli as pg
+    >>> from pygimli.meshtools import createRectangle
+    >>> rectangle = createRectangle(pos=[5, -5], size=[2, 2],
+    ...                             marker=4, area=0.1)
+    >>> _ = pg.show(rectangle)
     """
     # if not ((start and end) or (pos and size)):
     #     raise BaseException("createRectangle pls. give either start and end"
@@ -109,18 +120,39 @@ def createRectangle(start=None, end=None, pos=None, size=None, **kwargs):
     poly.createNode(ePos)
     poly.createNode([ePos[0], sPos[1]])
 
-    if kwargs.pop('isHole', False):
-        poly.addHoleMarker(sPos + (ePos - sPos) * 0.2)
-    else:
-        poly.addRegionMarker(sPos + (ePos - sPos) * 0.2,
-                             marker=kwargs.pop('marker', 1),
-                             area=kwargs.pop('area', 0))
+    markerposition = kwargs.pop('markerPosition', None)
 
+    def addMarker(markerposition):
+        # use defaults
+        if markerposition is None:
+            markerposition = sPos + (ePos - sPos) * 0.2
+
+        if kwargs.pop('isHole', False):
+            poly.addHoleMarker(markerposition)
+        else:
+            poly.addRegionMarker(
+                markerposition,
+                marker=kwargs.pop('marker', 1),
+                area=kwargs.pop('area', 0)
+            )
+
+    # if the user supplies an absolute marker position, we need to add it after
+    # transforming the polygon
+    marker_added = False
+    if markerposition is None:
+        addMarker(markerposition)
+        marker_added = True
+
+    # Note that we do not support the usage of start/end AND size/pos. Only one
+    # of the pairs. Otherwise strange things will happen with the region
+    # markers!
     if size is not None:
         poly.scale(size)
     if pos is not None:
         poly.translate(pos)
 
+    if not marker_added:
+        addMarker(markerposition)
     polyCreateDefaultEdges_(poly, **kwargs)
 
     return poly
@@ -146,7 +178,6 @@ def createWorld(start, end, marker=1, area=0., layers=None, worldMarker=True):
     area : float | list
         Maximum cell size for resulting triangles after mesh generation.
         If area is a float set it global, if area is a list set it per layer.
-
     layers : [float]
         Add some layers to the world.
     worldMarker : [bool]
@@ -187,11 +218,11 @@ def createWorld(start, end, marker=1, area=0., layers=None, worldMarker=True):
         n = poly.createNode([start[0], depth])
         if i > 0:
             if len(z) == 2:
-                poly.addRegionMarker(n.pos() + [0.2, 0.2], marker=marker,
-                                     area=area[0])
+                poly.addRegionMarker(
+                    n.pos() + [0.2, 0.2], marker=marker, area=area[0])
             else:
-                poly.addRegionMarker(n.pos() + [0.2, 0.2], marker=i,
-                                     area=area[i - 1])
+                poly.addRegionMarker(
+                    n.pos() + [0.2, 0.2], marker=i, area=area[i - 1])
 
     for i, depth in enumerate(z[::-1]):
         poly.createNode([end[0], depth])
@@ -385,7 +416,7 @@ def createPolygon(verts, isClosed=False, isHole=False, **kwargs):
     """Create a polygon from a list of vertices.
 
     All vertices need to be unique and duplicate vertices will be ignored.
-    If you want the polygon be a closed region you can set the 'isCloses' flag.
+    If you want the polygon be a closed region you can set the 'isClosed' flag.
     Closed region can be attributed by assigning a region marker.
     The automatic region marker is placed in the center of all vertices.
 
@@ -396,15 +427,18 @@ def createPolygon(verts, isClosed=False, isHole=False, **kwargs):
 
     **kwargs:
 
-        * boundaryMarker : int [1]
-            Marker for the resulting boundary edges
-        * leftDirection : bool [True]
-            Rotational direction
-        * marker : int [None]
+        marker : int [None]
             Marker for the resulting triangle cells after mesh generation.
-        * area : float [0]
+        markerPosition : floats [x, y] [0.0, 0.0]
+            Position (absolute) of the marker (works for both regions and
+            holes)
+        boundaryMarker : int [1]
+            Marker for the resulting boundary edges
+        leftDirection : bool [True]
+            Rotational direction
+        area : float [0]
             Maximum cell size for resulting triangles after mesh generation
-        * isHole : bool [False]
+        isHole : bool [False]
             The polygon will become a hole instead of a triangulation
 
     isClosed : bool [True]
@@ -440,15 +474,15 @@ def createPolygon(verts, isClosed=False, isHole=False, **kwargs):
                             boundaryMarker=boundaryMarker)
 
     if isClosed and marker is not None or area > 0:
+        markerposition = kwargs.pop(
+            'markerPosition', pg.center(poly.positions())
+        )
         if isHole:
-            poly.addHoleMarker(pg.center(poly.positions()))
+            poly.addHoleMarker(markerposition)
         else:
             if marker is None:  # in case marker is None but area is given
                 marker = 0
-            poly.addRegionMarker(pg.center(poly.positions()),
-                                 marker=marker, area=area)
-
-    # set a regionmarker here .. somewhere
+            poly.addRegionMarker(markerposition, marker=marker, area=area)
 
     pg.warnNonEmptyArgs(kwargs)
     return poly
