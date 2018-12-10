6c668405f78a664ad700325e9ae765dd91b38363
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-12-10 09:58:18 +0100

    add tests for createCircle and createPolygon, and check for marker numbers

--- python/pygimli/testing/test_polytools.py
+++ python/pygimli/testing/test_polytools.py
@@ -15,18 +15,20 @@ class TestCreateRectangle(object):
         # sPos + (ePos - sPos) * 0.2)
         assert rect1.regionMarker()[0].x() == 0.4
         assert rect1.regionMarker()[0].y() == -0.2
+        assert rect1.regionMarker()[0].marker() == 1
 
     def test_region_marker_position_translation_scale(self):
         rect1 = mt.createRectangle(
             pos=[1.0, 2.0],
             size=[2.0, 4],
             isClosed=True,
-            marker=1,
+            marker=20,
         )
         # by default the region marker should be located at
         # sPos + (ePos - sPos) * 0.2)
         assert rect1.regionMarker()[0].x() == 0.4
         assert rect1.regionMarker()[0].y() == 4 - 4 * 0.2
+        assert rect1.regionMarker()[0].marker() == 20
 
     def test_region_marker_position_two_ways_v1(self):
         rect1 = mt.createRectangle(
@@ -45,6 +47,9 @@ class TestCreateRectangle(object):
         )
         assert rect1.regionMarker()[0].x() == -1.2
         assert rect1.regionMarker()[0].y() == -0.6
+
+        assert rect1.regionMarker()[0].marker() == 1
+        assert rect2.regionMarker()[0].marker() == 2
 
         assert rect1.regionMarker()[0].x() == rect1.regionMarker()[0].x()
         assert rect2.regionMarker()[0].x() == rect2.regionMarker()[0].x()
@@ -70,6 +75,9 @@ class TestCreateRectangle(object):
         assert rect1.regionMarker()[0].x() == 4 * 0.2
         assert rect1.regionMarker()[0].y() == -1.5 - 2.5 * 0.2
 
+        assert rect1.regionMarker()[0].marker() == 1
+        assert rect2.regionMarker()[0].marker() == 2
+
         assert rect1.regionMarker()[0].x() == rect1.regionMarker()[0].x()
         assert rect2.regionMarker()[0].x() == rect2.regionMarker()[0].x()
 
@@ -81,11 +89,13 @@ class TestCreateRectangle(object):
             start=[0.0, -1.5],
             end=[4.0, -4.0],
             isClosed=True,
-            marker=1,
+            marker=10,
             markerPosition=[0.0, 0.0],
         )
         assert rect1.regionMarker()[0].x() == 0
         assert rect1.regionMarker()[0].y() == 0
+
+        assert rect1.regionMarker()[0].marker() == 10
 
     def test_region_markerposition_pos_size(self):
         # scaled version
@@ -93,8 +103,78 @@ class TestCreateRectangle(object):
             pos=[2.0, -2.75],
             size=[4.0, 2.5],
             isClosed=True,
-            marker=2,
+            marker=5,
             markerPosition=[0.0, 0.0],
         )
         assert rect1.regionMarker()[0].x() == 0
         assert rect1.regionMarker()[0].y() == 0
+        assert rect1.regionMarker()[0].marker() == 5
+
+
+class TestCreateCircle(object):
+    def test_default_create(self):
+        circle = mt.createCircle(
+            pos=[0.0, 0.0],
+            radius=1.0,
+            marker=1
+        )
+        assert circle.regionMarker()[0].x() == 0
+        assert circle.regionMarker()[0].y() == 0
+        assert circle.regionMarker()[0].marker() == 1
+
+    def test_default_create_with_scaling(self):
+        circle = mt.createCircle(
+            pos=[0.0, 0.0],
+            radius=3.0,
+            marker=6
+        )
+        assert circle.regionMarker()[0].x() == 0
+        assert circle.regionMarker()[0].y() == 0
+        assert circle.regionMarker()[0].marker() == 6
+
+    def test_create_with_custom_markerPosition(self):
+        circle = mt.createCircle(
+            pos=[0.0, 0.0],
+            radius=3.0,
+            marker=9,
+            markerPosition=[2.0, -1.0],
+        )
+        assert circle.regionMarker()[0].x() == 2.0
+        assert circle.regionMarker()[0].y() == -1
+        assert circle.regionMarker()[0].marker() == 9
+
+
+class TestCreatePolygon(object):
+    def test_default_create(self):
+        polygon = mt.createPolygon(
+            [[-1.0, -1.0], [-1.0, 1.0], [1.0, 1.0], [1.0, -1]],
+            isClosed=True,
+            marker=1,
+        )
+
+        assert polygon.regionMarker()[0].x() == 0
+        assert polygon.regionMarker()[0].y() == 0
+        assert polygon.regionMarker()[0].marker() == 1
+
+    def test_default_create_different_center(self):
+        polygon = mt.createPolygon(
+            [[0.0, 0.0], [1.0, 0.0], [1.0, -1.0], [0.0, -1]],
+            isClosed=True,
+            marker=2,
+        )
+
+        assert polygon.regionMarker()[0].x() == 0.5
+        assert polygon.regionMarker()[0].y() == -0.5
+        assert polygon.regionMarker()[0].marker() == 2
+
+    def test_create_with_custom_markerPosition(self):
+        polygon = mt.createPolygon(
+            [[0.0, 0.0], [1.0, 0.0], [1.0, -1.0], [0.0, -1]],
+            isClosed=True,
+            marker=7,
+            markerPosition=[0.1, -0.1],
+        )
+
+        assert polygon.regionMarker()[0].x() == 0.1
+        assert polygon.regionMarker()[0].y() == -0.1
+        assert polygon.regionMarker()[0].marker() == 7
