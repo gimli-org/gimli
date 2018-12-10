8cd17f7024d830bb2f70b181d50d0daedb4b4947
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-12-10 09:58:55 +0100

    update marker example: remove savefig calls and add fig.show() calls

--- doc/examples/1_meshing/plot_markers.py
+++ doc/examples/1_meshing/plot_markers.py
@@ -6,8 +6,8 @@ Region markers
 ==============
 
 This example highlights the usage of regionMarkers, as well as some properties
-of regions if the :py:module:`pygimli.meshtools` module is used to create
-complex compound meshes.
+of regions if the **pygimli.meshtools** package is used to create complex
+compound meshes.
 
 When constructing complex geometries out of basic geometric shapes (e.g.,
 circle, rectangle, ...) we need to be careful with the region markers and their
@@ -20,12 +20,13 @@ markers.
 import pygimli as pg
 import pygimli.meshtools as mt
 
-"""
 ###############################################################################
 # In this first part we naively combine objects and assign markers to them,
 # expecting two regions of concentric rings with markers 1 and 2. Note how the
 # outer ring is assigned the marker 0 in the figure, although we specified
-# marker=1 for the larger circle?
+# marker=1 for the larger circle? A marker value of 0 is assigned to a region
+# if no region marker is found, indicating that the marker for the outer ring
+# was overwritten/ignored by the inner circle, which was added later.
 circle_outer = mt.createCircle(
     pos=[0.0, 0.0],
     radius=3.0,
@@ -45,11 +46,11 @@ plc = mt.mergePLC([circle_outer, circle_inner])
 ax, cb = pg.show(plc, markers=True)
 
 ###############################################################################
-# The solution to this problem are the region marker, which define the marker
-# value of the region they are placed in. By default all region markers are
-# assigned the position (0,0,0), thereby overwriting each other (see dots in
-# figure below). If no region marker is present in a region, a marker value of
-# 0 is assigned.
+# The solution to this problem is the region marker, which defines the marker
+# value of the region that it is placed in. By default all region markers are
+# assigned the position (0,0,0), thereby overwriting each other (see black dots
+# in figure below). If no region marker is present in a region, a marker value
+# of 0 is assigned.
 
 fig = ax.get_figure()
 for nr, marker in enumerate(plc.regionMarker()):
@@ -57,13 +58,14 @@ for nr, marker in enumerate(plc.regionMarker()):
         'Position marker number {}:'.format(nr + 1),
         marker.x(), marker.y(), marker.z()
     )
-    ax.scatter(marker.x(), marker.y(), s=(2 - nr) * 20)
-ax.set_title('marker positions')
+    ax.scatter(marker.x(), marker.y(), s=(2 - nr) * 30, color='k')
+ax.set_title('marker positions - non-working example')
 fig.show()
 
 ###############################################################################
 # Let us fix this issue by assigning region marker positions that are not
-# overwritten by other objects when the geometries are merged:
+# overwritten by other objects when the geometries are merged (using the
+# **markerPosition** parameter):
 circle_outer = mt.createCircle(
     pos=[0.0, 0.0],
     radius=3.0,
@@ -88,11 +90,13 @@ for nr, marker in enumerate(plc.regionMarker()):
         'Position marker number {}:'.format(nr + 1),
         marker.x(), marker.y(), marker.z()
     )
-    ax.scatter(marker.x(), marker.y(), s=(2 - nr) * 20)
-ax.set_title('marker positions')
+    ax.scatter(marker.x(), marker.y(), s=(2 - nr) * 30, color='k')
+ax.set_title('marker positions - working example')
 fig.show()
-"""
+
 ###############################################################################
+# The same issue can occur for polygons. Polygons can assume complex forms, but
+# for simplicity we create cubes here.
 polygon1 = mt.createPolygon(
     [[0.0, 0.0], [1.0, 0.0], [1.0, -1.0], [0.0, -1]],
     isClosed=True,
@@ -116,10 +120,11 @@ for nr, marker in enumerate(plc.regionMarker()):
         marker.x(), marker.y(), marker.z()
     )
     ax.scatter(marker.x(), marker.y(), s=(4 - nr) * 20, color='k')
-ax.set_title('marker positions')
-fig.savefig('polygons_test_abs.pdf')
+ax.set_title('marker positions - non-working example')
+fig.show()
 
 ###############################################################################
+# Again, we can simply fix with the **markerPosition** parameter
 polygon1 = mt.createPolygon(
     [[0.0, 0.0], [1.0, 0.0], [1.0, -1.0], [0.0, -1]],
     isClosed=True,
@@ -144,79 +149,10 @@ for nr, marker in enumerate(plc.regionMarker()):
         marker.x(), marker.y(), marker.z()
     )
     ax.scatter(marker.x(), marker.y(), s=(4 - nr) * 20, color='k')
-ax.set_title('marker positions')
-fig.savefig('polygons_test_abs_fixed.pdf')
+ax.set_title('marker positions - working example')
+fig.show()
 ###############################################################################
-polygon1 = mt.createPolygon(
-    [[0.0, 0.0], [1.0, 0.0], [1.0, -1.0], [0.0, -1]],
-    isClosed=True,
-    marker=1,
-)
-
-polygon2 = mt.createPolygon(
-    [[1.0, -1.0], [2.0, -1.0], [2.0, -2.0], [1.0, -2.0]],
-    isClosed=True,
-    marker=2,
-)
-
-polygon3 = mt.createPolygon(
-    [[0.6, -0.5], [1.4, -0.5], [1.4, -1.5], [0.6, -1.5]],
-    isClosed=True,
-    marker=3,
-)
-
-plc = mt.mergePLC([polygon1, polygon2, polygon3])
-
-ax, cb = pg.show(plc, markers=True)
-
-fig = ax.get_figure()
-for nr, marker in enumerate(plc.regionMarker()):
-    print(
-        'Position marker number {}:'.format(nr + 1),
-        marker.x(), marker.y(), marker.z()
-    )
-    ax.scatter(marker.x(), marker.y(), s=(4 - nr) * 20, color='k')
-ax.set_title('marker positions')
-fig.savefig('polygons.pdf')
-###############################################################################
-rect1 = mt.createRectangle(
-    start=[0.0, 0.0],
-    end=[2.0, -1.0],
-    isClosed=True,
-    marker=1,
-)
-
-# move the rectangle by changing the center position
-rect2 = mt.createRectangle(
-    start=[4.0, 0.0],
-    end=[6.0, -1.0],
-    isClosed=True,
-    marker=2,
-)
-
-# scaled version
-rect3 = mt.createRectangle(
-    pos=[2.0, -2.75],
-    size=[4.0, 2.5],
-    isClosed=True,
-    marker=3,
-)
-
-plc = mt.mergePLC([rect1, rect2, rect3])
-
-ax, cb = pg.show(plc, markers=True)
-
-fig = ax.get_figure()
-for nr, marker in enumerate(plc.regionMarker()):
-    print(
-        'Position marker number {}:'.format(nr + 1),
-        marker.x(), marker.y(), marker.z()
-    )
-    ax.scatter(marker.x(), marker.y(), s=(4 - nr) * 20, color='k')
-ax.set_title('marker positions')
-fig.savefig('rect.pdf')
-
-###############################################################################
+# And finally, a similar example for rectangles...
 rect1 = mt.createRectangle(
     start=[0.0, 0.0],
     end=[2.0, -1.0],
@@ -243,10 +179,11 @@ for nr, marker in enumerate(plc.regionMarker()):
         marker.x(), marker.y(), marker.z()
     )
     ax.scatter(marker.x(), marker.y(), s=(4 - nr) * 20, color='k')
-ax.set_title('marker positions')
-fig.savefig('rect_bad.pdf')
+ax.set_title('marker positions - non-working example')
+fig.show()
 
 ###############################################################################
+# For the last time, fixing it...
 rect1 = mt.createRectangle(
     start=[0.0, 0.0],
     end=[2.0, -1.0],
@@ -274,9 +211,8 @@ for nr, marker in enumerate(plc.regionMarker()):
         marker.x(), marker.y(), marker.z()
     )
     ax.scatter(marker.x(), marker.y(), s=(4 - nr) * 20, color='k')
-ax.set_title('marker positions')
-fig.savefig('rect_fixed.pdf')
-
+ax.set_title('marker positions - working example')
+fig.show()
 
 ###############################################################################
 # .. note::
