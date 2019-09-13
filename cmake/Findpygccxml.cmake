d52e67915b99272da757d4520547c8fbe6bfc050
carsten-forty2 <carsten@pygimli.org>
2019-09-13 02:38:36 +0200

    try pygccxml and pyplusplus pip version if installed

--- cmake/Findpygccxml.cmake
+++ cmake/Findpygccxml.cmake
@@ -5,17 +5,26 @@
 # PYGCCXML_FOUND, If false, do not try to use pygccxml
 # PYPLUSPLUS_FOUND, If false, do not try to use pyplusplus
 
-find_path(PYGCCXML_PATH pygccxml/__init__.py
-    HINTS
-        ${EXTERNAL_DIR}
-    NO_DEFAULT_PATH
-)
 
-if (PYGCCXML_PATH)
-    message( STATUS "Found pygccxml path: ${PYGCCXML_PATH}")
+find_python_module(pygccxml)
+
+if (pygccxml_FOUND)
     set(PYGCCXML_FOUND TRUE)
-else (PYGCCXML_PATH)
-    message( STATUS "NOT Found pygccxml: cannot build pygimli.")
-endif(PYGCCXML_PATH)
+    set(PYGCCXML_PATH ${pygccxml_LOC})
+else()
+    find_path(PYGCCXML_PATH pygccxml/__init__.py
+        HINTS
+            ${EXTERNAL_DIR}
+        NO_DEFAULT_PATH
+    )
+
+    if (PYGCCXML_PATH)
+        message( STATUS "Found pygccxml path: ${PYGCCXML_PATH}")
+        set(PYGCCXML_FOUND TRUE)
+    else (PYGCCXML_PATH)
+        message( STATUS "NOT Found pygccxml: cannot build pygimli.")
+    endif(PYGCCXML_PATH)
+
+endif()
 
 mark_as_advanced(PYGCCXML)
