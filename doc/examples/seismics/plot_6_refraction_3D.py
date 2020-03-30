25f4b13c914c1c047240d089bb97f210e2742a06
Friedrich Dinsel <f.dinsel@gmail.com>
2020-03-30 09:16:56 +0200

    fixed seismic example

--- doc/examples/seismics/plot_6_refraction_3D.py
+++ doc/examples/seismics/plot_6_refraction_3D.py
@@ -81,7 +81,7 @@ for receiver in sensors[1:]:
 # Plot final ray paths.
 
 if pyvista:
-    plotter = pg.show(mesh, hold=True, label=label)
+    plotter, _ = pg.show(mesh, hold=True, label=label, alpha=0.1)
     points = pyvista.PolyData(sensors)
     plotter.add_mesh(points, point_size=10, color="k")
 
@@ -91,4 +91,4 @@ if pyvista:
             stop = tuple(ray[i + 1])
             line = pyvista.Line(start, stop)
             plotter.add_mesh(line, color='green', line_width=2)
-    plotter.plot()
+    plotter.show()
