5f528cc7e9e5e288bb10f2bc925a6b412250bbc7
Florian Wagner <mail@fwagner.info>
2020-05-05 17:00:14 +0200

    Minor

--- doc/examples/5_misc/plot_1_dcem.py
+++ doc/examples/5_misc/plot_1_dcem.py
@@ -14,6 +14,8 @@ what happens under the hood in the much simpler script that follows."""
 # The case has been documented by :cite:`Guenther2013NSG`.
 
 import numpy as np
+np.random.seed(1337)
+
 import matplotlib.pyplot as plt
 
 import pygimli as pg
