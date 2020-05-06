3ced063f5a1250b78901e68826ce8eab60855875
carsten-forty2 <carsten@pygimli.org>
2020-05-06 21:16:29 +0200

    fix example, remove old link to tutorial

--- doc/conf.py
+++ doc/conf.py
@@ -241,7 +241,7 @@ release = release.replace('_', '\\_')
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ['_build', '_sphinx-ext', '_templates', 'tmp', 'examples',
-                    'tutorials', 'tutorial']
+                    'tutorials' ]
 
 # The reST default role (used for this markup: `text`) to use for all documents
 # default_role = None
