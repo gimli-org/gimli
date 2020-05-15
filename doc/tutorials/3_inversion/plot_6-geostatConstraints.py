ffb49466591a2cfdb2382b595c160b4b7636f3d2
Thomas Günther <halbmy@users.noreply.github.com>
2020-05-15 08:16:59 +0200

    Update plot_6-geostatConstraints.py

--- doc/tutorials/3_inversion/plot_6-geostatConstraints.py
+++ doc/tutorials/3_inversion/plot_6-geostatConstraints.py
@@ -14,9 +14,9 @@ by the distances H between the model cells i and j into the three directions
 .. math::
 
     \textbf{C}_{\text{M},ij}=\sigma^{2}\exp{\left(
-        -3\sqrt{\left(\frac{\textbf{H}_{x,ij}}{I_{x}}\right)^{2}+
-                \left(\frac{\textbf{H}_{y,ij}}{I_{y}}\right)^{2}+
-                \left(\frac{\textbf{H}_{z,ij}}{I_{z}}\right)^{2}}\right)}.
+        -3\sqrt{\left(\frac{\textbf{H}^x_{ij}}{I_{x}}\right)^{2}+
+                \left(\frac{\textbf{H}^y_{ij}}{I_{y}}\right)^{2}+
+                \left(\frac{\textbf{H}^z_{ij}}{I_{z}}\right)^{2}}\right)}.
 
 It defines the correlation between model cells as a function of correlation
 lenghts (ranges) :math:`I_x`, :math:`I_y`, and :math:`I_z`. Of course, the
@@ -24,7 +24,10 @@ orientation of the coordinate axes is arbitrary and can be chosen by rotation.
 Let us illustrate this by a simple mesh:
 """
 
-# %% create a simple mesh (e.g. a crosshole box geometry)
+# %% 
+# Computing covariance and constraint matrices
+# --------------------------------------------
+# We create a simple mesh using a box geometry
 import pygimli as pg
 import pygimli.meshtools as mt
 # We create a rectangular domain and mesh it with small triangles
@@ -32,19 +35,24 @@ rect = mt.createRectangle(start=[0, -10], end=[10, 0])
 mesh = mt.createMesh(rect, quality=34.5, area=0.1)
 
 # %%
-# We can compute this covariance matrix by calling
-CM = pg.matrix.covarianceMatrix(mesh, I=5)
+# We compute such a covariance matrix by calling
+CM = pg.matrix.covarianceMatrix(mesh, I=5)  # I taken for both x and y
 # We search for the cell where the midpoint (5, -5) is located in
 ind = mesh.findCell([5, -5]).id()
-# and plot the according column
-# col = pg.log10(CM.dot(vec))
+# and plot the according column using index access (numpy)
 pg.show(mesh, CM[:, ind], cMap="magma_r");
 
 # %%
 # According to inverse theory, we use the square root of the covariance matrix
 # as single-side regularization matrix C. It is computed by using an eigenvalue
-# decomposition based on the numpy linalg procedure
+# decomposition
 #
+# .. math::
+#
+#     \textbf{C}_\text{M} = \textbf{Q}\textbf{D}\textbf{Q}^{T}
+#
+# based on LAPACK (numpy.linalg). The inverse square root is defined by
+# 
 # .. math::
 #
 #     \textbf{C}_\text{M}^{-0.5} = \textbf{Q}\textbf{D}^{-0.5}\textbf{Q}^{T}
@@ -57,7 +65,7 @@ pg.show(mesh, CM[:, ind], cMap="magma_r");
 
 Cm05 = pg.matrix.Cm05Matrix(CM)
 # %%
-# This matrix does not return a zero vector for a constant vector
+# However, this matrix does not return a zero vector for a constant vector
 out = Cm05 * pg.Vector(mesh.cellCount(), 1.0)
 print(min(out), max(out))
 
@@ -75,7 +83,7 @@ C = pg.matrix.GeostatisticConstraintsMatrix(mesh=mesh, I=5)
 # In order to extract a certain column, we generate a vector with a single 1
 vec = pg.Vector(mesh.cellCount())
 vec[ind] = 1.0
-pg.show(mesh, pg.log10(pg.abs(C*vec)), cMin=-6, cMax=0, cMap="magma_r");
+ax, cb = pg.show(mesh, pg.log10(pg.abs(C*vec)), cMin=-6, cMax=0, cMap="magma_r")
 
 # %%
 # The constraints have a rather small footprint compared to the correlation
@@ -83,9 +91,9 @@ pg.show(mesh, pg.log10(pg.abs(C*vec)), cMin=-6, cMax=0, cMap="magma_r");
 # constraint matrices that only include relations to neighboring cells.
 
 # %%
-# Such a matrix can also be defined for different ranges and a dip angle
+# Such a matrix can also be defined for different ranges and dip angles, e.g.
 Cdip = pg.matrix.GeostatisticConstraintsMatrix(mesh=mesh, I=[10, 3], dip=-25)
-pg.show(mesh, pg.log10(pg.abs(Cdip*vec)), cMin=-6, cMax=0, cMap="magma_r");
+ax, cb = pg.show(mesh, pg.log10(pg.abs(Cdip*vec)), cMin=-6, cMax=0, cMap="magma_r")
 
 # %%
 # In order to illustrate the role of the constraints, we use a very simple
@@ -123,6 +131,8 @@ class PriorFOP(pg.core.ModellingBase):
 
 
 # %%
+# Inversion with geostatistical constraints
+# -----------------------------------------
 # We choose some positions and initialize the forward operator
 pos = [[2, -2], [8, -2], [5, -5], [2, -8], [8, -8]]
 fop = PriorFOP(mesh, pos)
@@ -139,7 +149,6 @@ kw = dict(
 startModel = pg.Vector(mesh.cellCount(), 30)
 tLog = pg.core.TransLog()
 vals = [30, 50, 300, 100, 200]
-# vals = [10, 20, 50, 30, 40]
 inv = pg.core.Inversion(vals, fop, tLog, tLog)
 inv.setRelativeError(0.05)  # 5 % error
 inv.setModel(startModel)
@@ -148,19 +157,19 @@ inv.setLambda(200)
 fop.regionManager().setConstraintType(2)
 res = inv.run()
 print(('{:.1f} ' * 5).format(*fop(res)), inv.chi2())
-pg.show(mesh, res, ax=ax[0, 1], **kw)
+ax, cb = pg.show(mesh, res, ax=ax[0, 1], **kw)
 # Next, we use first-order constraints
 fop.regionManager().setConstraintType(1)
 res = inv.run()
 print(('{:.1f} ' * 5).format(*fop(res)), inv.chi2())
-pg.show(mesh, res, ax=ax[0, 0], **kw)
+ax, cb = pg.show(mesh, res, ax=ax[0, 0], **kw)
 # Now we set the geostatistic isotropic operator with 5m correlation length
 fop.setConstraints(C)
 inv.setModel(startModel)
 inv.setLambda(30)
 res = inv.run()
 print(('{:.1f} ' * 5).format(*fop(res)), inv.chi2())
-pg.show(mesh, res, ax=ax[1, 0], **kw)
+ax, cb = pg.show(mesh, res, ax=ax[1, 0], **kw)
 ax[0, 0].set_title("1st order")
 ax[0, 1].set_title("2nd order")
 ax[1, 0].set_title("I=5")
@@ -170,12 +179,13 @@ inv.setLambda(20)
 inv.setModel(startModel)
 res = inv.run()
 print(('{:.1f} ' * 5).format(*fop(res)), inv.chi2())
-pg.show(mesh, res, ax=ax[1, 1], **kw)
+ax, cb = pg.show(mesh, res, ax=ax[1, 1], **kw)
 ax[1, 1].set_title("I=[10/3], dip=25")
 # plot the position of the priors
 for ai in ax.flat:
     for po in pos:
         ai.plot(*po, marker='o', markersize=10, color='k', fillstyle='none')
+#
 # %%
 # Generating geostatistical media
 # -------------------------------
@@ -184,4 +194,4 @@ for ai in ax.flat:
 # it with a pseudo-random (randn) series. The arguments are the same as for the
 # correlation or constraint matrices.
 model = pg.utils.generateGeostatisticalModel(mesh, I=[20, 4])
-pg.show(mesh, model)
+ax, cb = pg.show(mesh, model)
