1852f11169f45e3dd6b798c5ae9e8c33c7bef6ae
Thomas Günther <halbmy@users.noreply.github.com>
2020-09-28 12:49:43 +0200

    Replace numpy.random.randn by pg.randn(len)

--- doc/examples/5_misc/plot_1_dcem.py
+++ doc/examples/5_misc/plot_1_dcem.py
@@ -14,8 +14,6 @@ what happens under the hood in the much simpler script that follows."""
 # The case has been documented by :cite:`Guenther2013NSG`.
 
 import numpy as np
-np.random.seed(1337)
-
 import matplotlib.pyplot as plt
 
 import pygimli as pg
@@ -70,7 +68,7 @@ for i in range(nf-1):
 
 fEM = pg.core.FDEM1dModelling(nlay, freq, coilspacing)
 dataEM = fEM(model)
-dataEM += np.random.randn(len(dataEM)) * noiseEM
+dataEM += pg.randn(len(dataEM), seed=1234) * noiseEM
 
 ###############################################################################
 # We define model transformations: logarithms and log with upper+lower bounds
@@ -104,7 +102,7 @@ for i in range(na-1):
     ab2[i+1] = ab2[i] * 1.3
 fDC = pg.core.DC1dModelling(nlay, ab2, mn2)
 dataDC = fDC(model)
-dataDC *= 1. + np.random.randn(len(dataDC)) * noiseDC / 100.
+dataDC *= 1. + pg.randn(len(dataDC), seed=1234) * noiseDC / 100.
 
 fDC.region(0).setTransModel(transThk)
 fDC.region(1).setTransModel(transRes)
