5fc2be8390626e96433a347034de4239a4462388
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2022-06-30 12:48:26 +0200

    add CGLS algorithm

--- pygimli/solver/leastsquares.py
+++ pygimli/solver/leastsquares.py
@@ -4,11 +4,10 @@ from numpy.linalg import norm
 import pygimli as pg
 
 
-def lsqr(A, b, damp=0.0, x=None, maxiter=200, verbose=False):
+def lsqr(A, b, x=None, maxiter=200, tol=1e-8, verbose=False, damp=0.0):
     """Solve A x = b in a Least-Squares sense using LSQR algorithm.
 
     After Page and Saunders (1982)
-
 
     Parameters
     ==========
@@ -22,6 +21,8 @@ def lsqr(A, b, damp=0.0, x=None, maxiter=200, verbose=False):
         damping value for very ill-conditioned systems
     maxiter : int [200]
         maximum iteration number
+    tol : float [1e-8]
+        solution tolerance
     verbose : bool [False]
         print out convergence every 10th iteration
 
@@ -32,7 +33,7 @@ def lsqr(A, b, damp=0.0, x=None, maxiter=200, verbose=False):
     """
     if x is None:  # no starting vector
         x = pg.Vector(A.cols())
-        u = b
+        u = b.copy()
     else:
         u = b - A.mult(x)
 
@@ -72,10 +73,73 @@ def lsqr(A, b, damp=0.0, x=None, maxiter=200, verbose=False):
         x += (phi/rho) * w
         w = v - (theta/rho) * w
         Arnorm = phiU * alfa * abs(c)
-        if Arnorm / Arnorm0 < 1e-8:
+        if Arnorm / Arnorm0 < tol:
             if verbose:
                 print("Solution norm reached")
                 print(i, Arnorm, Arnorm/Arnorm0)
+
+            break
+
+    return x
+
+
+def cgls(A, b, x=None, maxiter=200, tol=1e-8, verbose=False, damp=0.0):
+    """Solve A x = b in a Least-Squares sense using conjugate gradients (CGLS).
+
+    After Page and Saunders (1982)
+
+    Parameters
+    ==========
+    A : pg.MatrixBase or derived class
+        matrix (typically Jacobian and constraint matrix)
+    b : pg.Vector
+        right-hand-side vector (typically data misfit and model roughness)
+    x : pg.Vector [zero vector]
+        starting vector
+    damp : float [0.0]
+        damping value for very ill-conditioned systems
+    maxiter : int [200]
+        maximum iteration number
+    tol : float [1e-8]
+        solution tolerance
+    verbose : bool [False]
+        print out convergence every 10th iteration
+
+    Returns
+    =======
+    x : pg.Vector
+        solution x for A^T A x = A^T b
+    """
+    if x is None:  # no starting vector
+        x = pg.Vector(A.cols())
+        r = b.copy()
+    else:
+        r = b - A.mult(x)
+
+    s = A.transMult(r)
+    p = s.copy()
+    gamma = pg.math.dot(s, s)
+    rr = pg.math.dot(r, r)
+    rr0 = rr
+    for i in range(maxiter):
+        if verbose and (i % 10 == 0):
+            print(i, rr, rr/rr0)
+
+        q = A.mult(p)
+        alfa = gamma / pg.math.dot(q, q)
+        x += p * alfa
+        r -= q * alfa
+        s = A.transMult(r)
+        newgamma = pg.math.dot(s, s)
+        beta = newgamma / gamma
+        gamma = newgamma
+        p *= beta
+        p += s
+        rr = pg.math.dot(r, r)
+        if rr / rr0 < tol:
+            if verbose:
+                print("Solution norm reached")
+                print(i, rr, rr/rr0)
 
             break
 
