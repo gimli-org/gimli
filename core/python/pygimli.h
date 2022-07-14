99ec0372704e5ac4f6ce76b10314e1bb04568ee7
carsten-forty2 <carsten@pygimli.org>
2022-07-14 18:38:16 +0200

    resolve merge conflicts

--- core/python/pygimli.h
+++ core/python/pygimli.h
@@ -304,11 +304,8 @@ DEFINE_XVECTOR_STUFF__(RVector) //RVector last since auto rhs conversion will fa
     template CSparseMatrix operator * (const CSparseMatrix & A, const GIMLI::Complex & b);
 
     template class ElementMatrix< double >;
-<<<<<<< HEAD
-=======
-    template std::ostream & operator << (std::ostream & str,
-                                         const ElementMatrix< double > & p);
->>>>>>> dev
+    // template std::ostream & operator << (std::ostream & str,
+    //                                      const ElementMatrix< double > & p);
 
     template RVector unique(const RVector & a);
     // template IndexArray unique(const IndexArray & a);
