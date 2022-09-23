43ab43bc60dac83a80b8a1063a0d269a7894f30f
carsten-forty2 <carsten@pygimli.org>
2022-09-24 01:00:48 +0200

    jenkins

--- core/python/generate_pygimli_code.py
+++ core/python/generate_pygimli_code.py
@@ -404,12 +404,12 @@ def generate(defined_symbols, extraIncludes):
                 'ElectrodeShapeNodesWithBypass',
                 'FunctionDD',
                 'H2SparseMapMatrix',
-                # '::GIMLI::VectorIterator<GIMLI::Pos>', needed
-                '::GIMLI::VectorIterator<bool>',
-                '::GIMLI::VectorIterator<double>',
-                #'::GIMLI::VectorIterator<long>', needed
-                '::GIMLI::VectorIterator<unsigned long>',
-                '::GIMLI::VectorIterator<std::complex<double> >', 
+                #'::GIMLI::VectorIterator<GIMLI::Pos>', # needed
+                #'::GIMLI::VectorIterator<bool>',
+                #'::GIMLI::VectorIterator<double>',
+                #'::GIMLI::VectorIterator<long>',
+                # ::GIMLI::VectorIterator<unsigned long>', needed
+                #'::GIMLI::VectorIterator<std::complex<double> >', 
                   ]
             )
 
