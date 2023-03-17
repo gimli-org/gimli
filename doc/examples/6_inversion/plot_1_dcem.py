b7edc99bb36eb9fa04274605ac4fc4ee9720b360
Thomas Günther <halbmy@pygimli.org>
2023-03-17 09:07:29 +0100

    DOC: move DC+EM joint inversion to modern frameworks

--- doc/examples/6_inversion/plot_1_dcem.py
+++ doc/examples/6_inversion/plot_1_dcem.py
@@ -12,180 +12,147 @@ what happens under the hood in the much simpler script that follows."""
 
 ###############################################################################
 # The case has been documented by :cite:`Guenther2013NSG`.
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 import pygimli as pg
+from pygimli.physics.em import HEMmodelling
+from pygimli.physics.ves import VESModelling
+from pygimli.frameworks import JointModelling
+from pygimli.frameworks import MarquardtInversion
 from pygimli.viewer.mpl import drawModel1D
 
 
 ###############################################################################
-# First, we define a modelling class that calls two other classes and pastes
-# their results to one vector.
-class DCEM1dModelling(pg.core.ModellingBase):
-    """Modelling jointing DC and EM 1Dforward operators."""
-
-    def __init__(self, nlay, ab2, mn2, freq, coilspacing, verbose=False):
-        """Init number of layers, AB/2, MN/2, frequencies & coil spacing."""
-        pg.core.ModellingBase.__init__(self, verbose)
-        self.nlay_ = nlay
-        self.fDC_ = pg.core.DC1dModelling(nlay, ab2, mn2, verbose)
-        self.fEM_ = pg.core.FDEM1dModelling(nlay, freq, coilspacing, verbose)
-        self.mesh_ = pg.meshtools.createMesh1DBlock(nlay)
-        self.setMesh(self.mesh_)
-
-    def response(self, model):
-        """Return concatenated response of DC and EM FOPs."""
-        return pg.cat(self.fDC_(model), self.fEM_(model))
-
-
-###############################################################################
 # The actual script starts here. There are some options to play with
-noiseEM = 1.  # absolute (per cent of primary signal)
-noiseDC = 3.  # in per cent
-lamEM, lamDC, lamDCEM = 300., 500., 500.  # regularization strength
-verbose = False
+errorEMabs = 1.  # absolute (ppm of primary signal)
+errorDCrel = 3.  # in per cent
 
 ###############################################################################
 # First we create a synthetic model.
 
-nlay = 3  # number of layers
-thk = pg.Vector(nlay - 1, 15.0)  # 15m thickness each
-res = pg.Vector(nlay, 200.0)  # 200 Ohmm
-res[1] = 10.
-res[2] = 50.
-model = pg.cat(thk, res)  # paste together to one model
+nlay = 4  # number of layers
+synThk = [5, 15, 15]
+synRes = [1000, 100, 500, 20]
 
-###############################################################################
+# %%%
 # We first set up EM forward operator and generate synthetic data with noise
+#
 
-coilspacing = 50.
+# MaxMin-10/Promys instrument, 1m above the ground
 nf = 10
-freq = pg.Vector(nf, 110.)
-for i in range(nf-1):
-    freq[i+1] = freq[i] * 2.
+freq = 2**np.arange(nf) * 110.
+fEM = HEMmodelling(nlay=nlay, height=1, f=freq, r=100, scaling="%")
+# alternative: RESOLVE airborne system
+# freq = [338]
+# fEM = HEMmodelling(nlay=nlay, height=50, f=freq, r=10)
 
-fEM = pg.core.FDEM1dModelling(nlay, freq, coilspacing)
-dataEM = fEM(model)
-dataEM += pg.randn(len(dataEM), seed=1234) * noiseEM
+dataEM = fEM(synThk + synRes)
+errorEM = np.ones_like(dataEM) * errorEMabs
+dataEM += pg.randn(len(dataEM), seed=1234) * errorEM
+print(dataEM)
 
-###############################################################################
+# %%%
 # We define model transformations: logarithms and log with upper+lower bounds
+#
 
-transRhoa = pg.trans.TransLog()
-transThk = pg.trans.TransLog()
-transRes = pg.trans.TransLogLU(1., 1000.)
-transEM = pg.trans.Trans()
-fEM.region(0).setTransModel(transThk)
-fEM.region(1).setTransModel(transRes)
+# transRhoa = pg.trans.TransLog()
+# transEM = pg.trans.Trans()
 
 ###############################################################################
 # We set up the independent EM inversion and run the model.
+startModel = [10]*(nlay-1) + [100]*nlay
 
-invEM = pg.core.Inversion(dataEM, fEM, transEM, True, True)
-modelEM = pg.Vector(nlay * 2 - 1, 50.)
-invEM.setModel(modelEM)
-invEM.setAbsoluteError(noiseEM)
-invEM.setLambda(lamEM)
-invEM.setMarquardtScheme(0.9)
-modelEM = invEM.run()
-respEM = invEM.response()
+invEM = MarquardtInversion(fop=fEM, verbose=False)
+# invEM.setRegularization(0, limits=[1, 30], cType=0)
+# invEM.setRegularization(1, limits=[10, 1000])
+modelEM = invEM.run(dataEM, np.abs(errorEM/dataEM), startModel=startModel)
 
-###############################################################################
+# %%%
 # Next we set up the DC forward operator and generate synthetic data with noise
+#
 
 ab2 = pg.Vector(20, 3.)
 na = len(ab2)
 mn2 = pg.Vector(na, 1.0)
 for i in range(na-1):
     ab2[i+1] = ab2[i] * 1.3
-fDC = pg.core.DC1dModelling(nlay, ab2, mn2)
-dataDC = fDC(model)
-dataDC *= 1. + pg.randn(len(dataDC), seed=1234) * noiseDC / 100.
+fDC = VESModelling(ab2=ab2, mn2=mn2)
+dataDC = fDC(synThk+synRes)
+errorDC = np.ones_like(dataDC) * errorDCrel / 100.
+dataDC *= 1. + pg.randn(len(dataDC), seed=1234) * errorDC
 
-fDC.region(0).setTransModel(transThk)
-fDC.region(1).setTransModel(transRes)
+# fDC.region(0).setTransModel(transThk)
+# fDC.region(1).setTransModel(transRes)
 
 # We set up the independent DC inversion and let it run.
-invDC = pg.core.Inversion(dataDC, fDC, transRhoa, verbose)
-modelDC = pg.Vector(nlay*2-1, 20.)
-invDC.setModel(modelDC)
-invDC.setRelativeError(noiseDC/100.)
-invDC.setLambda(lamDC)
-invDC.setMarquardtScheme(0.9)
-modelDC = invDC.run()
-respDC = invDC.response()
+invDC = MarquardtInversion(fop=fDC, verbose=False)
+# invDC.dataTrans = pg.trans.TransLog()
+# invDC.setRegularization(0, limits=[1, 100], cType=0)
+# invDC.setRegularization(1, limits=[1, 1000], cType=0)
+modelDC = invDC.run(dataDC, errorDC, startModel=startModel)
 
-###############################################################################
-# Next we create a the joint forward operator (see class above).
+# %%%
+# We create a the joint forward operator using the Joint inversion framework.
+#
 
-fDCEM = DCEM1dModelling(nlay, ab2, mn2, freq, coilspacing)
-fDCEM.region(0).setTransModel(transThk)
-fDCEM.region(1).setTransModel(transRes)
+fDCEM = JointModelling([fDC, fEM])
+fDCEM.setData([dataDC, dataEM])  # just for sizes!
+# transDCEM = pg.trans.TransCumulative()
+# transDCEM.add(transRhoa, na)
+# transDCEM.add(transEM, nf*2)
+jointData = pg.cat(dataDC, dataEM)
+jointError = pg.cat(errorDC, np.abs(errorEM/dataEM))
+invDCEM = MarquardtInversion(fop=fDCEM, verbose=False)
+# invDCEM.setRegularization(0, limits=[1, 100], cType=0)
+# invDCEM.setRegularization(1, limits=[1, 1000], cType=0)
+modelDCEM = invDCEM.run(jointData, jointError,
+                        startModel=startModel)
 
-###############################################################################
-# We setup the joint inversion combining, transformations, data and errors.
-
-transData = pg.trans.TransCumulative()
-transData.add(transRhoa, na)
-transData.add(transEM, nf*2)
-invDCEM = pg.core.Inversion(pg.cat(dataDC, dataEM), fDCEM, transData, verbose)
-modelDCEM = pg.Vector(nlay * 2 - 1, 20.)
-invDCEM.setModel(modelDCEM)
-err = pg.cat(dataDC * noiseDC / 100., pg.Vector(len(dataEM), noiseEM))
-invDCEM.setAbsoluteError(err)
-invDCEM.setLambda(lamDCEM)
-invDCEM.setMarquardtScheme(0.9)
-modelDCEM = invDCEM.run()
-respDCEM = invDCEM.response()
-
-###############################################################################
+# %%%
 # The results of the inversion are plotted for comparison.
+#
 
 for inv in [invEM, invDC, invDCEM]:
     inv.echoStatus()
+
 print([invEM.chi2(), invDC.chi2(), invDCEM.chi2()])  # chi-square values
 
-###############################################################################
-# %% We finally plot the results
-fig = plt.figure(1, figsize=(10, 5))
-ax1 = fig.add_subplot(131)
-drawModel1D(ax1, thk, res, plot='semilogx', color='blue')
-drawModel1D(ax1, modelEM[0:nlay-1], modelEM[nlay-1:nlay*2-1], color='green')
-drawModel1D(ax1, modelDC[0:nlay-1], modelDC[nlay-1:nlay*2-1], color='cyan')
-drawModel1D(ax1, modelDCEM[0:nlay-1], modelDCEM[nlay-1:nlay*2-1],
-            color='red')
-ax1.legend(('syn', 'EM', 'DC', 'JI'))
+# %%%
+# We finally plot the results along with data and model responses.
+#
+
+fig, (ax1, ax2, ax3) = plt.subplots(figsize=(10, 5), ncols=3)
+drawModel1D(ax1, synThk, synRes, plot='semilogx', color='C0', label="synth")
+drawModel1D(ax1, model=modelEM, color='C1', label="DC")
+drawModel1D(ax1, model=modelDC, color='C2', label="EM")
+drawModel1D(ax1, model=modelDCEM, color='C3', label="DC-EM")
+ax1.legend()
 ax1.set_xlim((10., 1000.))
 ax1.set_ylim((40., 0.))
 ax1.grid(which='both')
-ax2 = fig.add_subplot(132)
-ax2.semilogy(dataEM[0:nf], freq, 'bx', label='syn IP')
-ax2.semilogy(dataEM[nf:nf*2], freq, 'bo', label='syn OP')
-ax2.semilogy(respEM[0:nf], freq, 'g--', label='EM')
-ax2.semilogy(respEM[nf:nf*2], freq, 'g--')
-ax2.semilogy(respDCEM[na:na+nf], freq, 'r:', label='DCEM')
-ax2.semilogy(respDCEM[na+nf:na+nf*2], freq, 'r:')
+ax2.semilogy(dataEM[0:nf], freq, 'x', color="C0", label='syn IP')
+ax2.semilogy(dataEM[nf:nf*2], freq, 'o', color="C0", label='syn OP')
+ax2.semilogy(invEM.response[0:nf], freq, '--', color="C2", label='EM')
+ax2.semilogy(invEM.response[nf:nf*2], freq, '--', color="C2")
+ax2.semilogy(invDCEM.response[na:na+nf], freq, ':', color="C2", label='DCEM')
+ax2.semilogy(invDCEM.response[na+nf:na+nf*2], freq, '2:', color="C2")
 ax2.set_ylim((min(freq), max(freq)))
 ax2.set_xlabel("IP/OP in %")
 ax2.set_ylabel("$f$ in Hz")
 ax2.yaxis.set_label_position("right")
 ax2.grid(which='both')
 ax2.legend(loc="best")
-ax3 = fig.add_subplot(133)
 ax3.loglog(dataDC, ab2, 'bx-', label='syn')
-ax3.loglog(respDC, ab2, 'c-', label='DC')
-ax3.loglog(respDCEM[0:na], ab2, 'r:', label='DCEM')
-# ax3.axis('tight')
+ax3.loglog(invDC.response, ab2, '-', label='DC', color="C1")
+ax3.loglog(invDCEM.response[0:na], ab2, '-', label='DCEM', color="C3")
 ax3.set_ylim((max(ab2), min(ab2)))
 ax3.grid(which='both')
 ax3.set_xlabel(r"$\rho_a$ in $\Omega$m")
 ax3.set_ylabel("AB/2 in m")
 ax3.yaxis.set_ticks_position("right")
 ax3.yaxis.set_label_position("right")
-ax3.legend(loc="best")
-pg.wait()
+ax3.legend()
 
 ###############################################################################
 
