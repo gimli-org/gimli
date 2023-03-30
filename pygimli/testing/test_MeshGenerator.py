aff9c9f6531f398b0b3eae8d31eaf72043a8c2c2
Florian Wagner <mail@fwagner.info>
2023-03-30 14:07:07 +0200

    TST: Make new mesh generator test work on Mac

--- pygimli/testing/test_MeshGenerator.py
+++ pygimli/testing/test_MeshGenerator.py
@@ -25,23 +25,26 @@ class TestMeshGenerator(unittest.TestCase):
         self.assertEqual(mesh.boundaryCount(), 5)
 
     def test_triangle_MAC(self):
-        """ Ther seems to be an issue on mac for higher quality, which produce 
-            different meshes on mac vs. linux/windows. Need observation and/or 
-            clearfication."""
+        """ There seems to be an issue on Mac for higher quality, which produce 
+            different meshes on mac vs. Linux/Windows. Needs observation and/or 
+            clarfication."""
         import pygimli as pg
         import pygimli.meshtools as mt
         plc = mt.createCircle(nSegments=24)
         l = mt.createLine(start=[0, -1], end=[0, -0.1], boundaryMarker=2)
         mesh = mt.createMesh([plc, l], area=0.1, quality=30)
         print(mesh)
-        #on Linux Mesh: Nodes: 43 Cells: 60 Boundaries: 102
-        #on Mac ??
+        # On Linux and Mac: Mesh: Nodes: 43 Cells: 60 Boundaries: 102
         self.assertEqual(mesh.nodeCount(), 43)
+
         mesh = mt.createMesh([plc, l], area=0.1, quality=32)
         print(mesh)
-        #on Linux Mesh: Nodes: 43 Cells: 60 Boundaries: 102
-        #on Mac ??
-        self.assertEqual(mesh.nodeCount(), 43)
+        if sys.platform == "darwin":
+            # On Mac: Mesh: Nodes: 46 Cells: 66 Boundaries: 111
+            self.assertEqual(mesh.nodeCount(), 46)
+        else:
+            # On Linux Mesh: Nodes: 43 Cells: 60 Boundaries: 102 (same as for quality=30)
+            self.assertEqual(mesh.nodeCount(), 43)
         
     def test_createGrid(self):
         mesh = pg.createGrid(3)
