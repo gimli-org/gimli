89c40b38301f344e41337f635f186467ee353cfc
Florian Wagner <mail@fwagner.info>
2023-04-04 12:47:05 +0200

    BLD: Make conda build work on Mac OS

--- core/python/generate_pygimli_code.py
+++ core/python/generate_pygimli_code.py
@@ -199,7 +199,6 @@ def generate(defined_symbols, extraIncludes):
     compiler_path = options.clang
 
     cflags = '-std=c++11'
-    cflags = ''
 
     if platform.system() == 'Windows':
         if platform.architecture()[0] == '64bit':
