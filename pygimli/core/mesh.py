886df5f678230c9031542a249489de00cd9a3aef
carsten-forty2 <carsten@pygimli.org>
2023-04-18 12:55:21 +0200

    transfer

--- pygimli/core/mesh.py
+++ pygimli/core/mesh.py
@@ -531,7 +531,9 @@ Mesh.findPaths = __Mesh_findPaths__
 def __Mesh_cutBoundary__(self, marker, boundaryMarker=None):
     """Cut the mesh along a given inner boundary.
 
-    Cut the mesh along a given boundary and convert this inner boundary to an outer. There will be new nodes to cut the connection between neighbouring cells. The new boundary can have an optional boundaryMarker.
+    Cut the mesh along a given boundary and convert this inner boundary to an outer. 
+    There will be new nodes to cut the connection between neighbouring cells. 
+    The new boundary can have an optional boundaryMarker.
 
     Restrictions
     ------------
@@ -558,19 +560,27 @@ def __Mesh_cutBoundary__(self, marker, boundaryMarker=None):
     >>> import pygimli as pg
     >>> import pygimli.meshtools as mt
     >>> plc = mt.createCircle(nSegments=24)
-    >>> l = mt.createLine(start=[0, -1], end=[0, -0.1], boundaryMarker=2)
-    >>> mesh = mt.createMesh([plc, l], area=0.1)
+    >>> l1 = mt.createLine(start=[0, -1], end=[0, -0.1], boundaryMarker=2)
+    >>> l2 = mt.createLine(start=[-0.3, 0.25], end=[0.3, 0.25], boundaryMarker=4)
+    >>> mesh = mt.createMesh([plc, l1, l2], area=0.1)
     >>> fig, axs= pg.plt.subplots(1, 2)
     >>> ax ,_ = pg.show(mesh, boundaryMarkers=True, ax=axs[0])
     >>> oldNodeCount = mesh.nodeCount()
     >>> print(mesh)
     Mesh: Nodes: 43 Cells: 60 Boundaries: 102
     >>> mesh.cutBoundary(marker=2, boundaryMarker=3)
+    >>> mesh.cutBoundary(marker=4, boundaryMarker=5)
     >>> print(mesh)
-    Mesh: Nodes: 46 Cells: 60 Boundaries: 105
-    >>> ## just move the new nodes little rightwards to see the cut
+    Mesh: Nodes: 50 Cells: 74 Boundaries: 123
+    >>> ## just move the new nodes a little to see the cuts
     >>> for n in range(oldNodeCount, mesh.nodeCount()):
-    ...     mesh.node(n).setPos(mesh.node(n).pos() + [0.1, 0.0])
+    ...     no = mesh.node(n)
+    ...     pg._g(no)
+    ...     for b in no.boundSet():
+    ...         if b.marker() == 3 or b.marker() == 5:
+    ...              pg._b(b)                  
+    ...              no.translate(b.norm()*-0.1)
+    ...              break;
     >>> ax, _ = pg.show(mesh, data=range(mesh.cellCount()),
     ...                 boundaryMarkers=True, colorBar=False,
     ...                 showMesh=True, boundaryProps={'lw':2}, ax=axs[1])
@@ -669,10 +679,21 @@ def __Mesh_cutBoundary__(self, marker, boundaryMarker=None):
         # pg._y(b.node(0).id(), b.node(1).id(), 'N', nA1.id(), nB1.id(), ':', lC.id(), rC.id())
 
         ### only if on outer boundary .. need check!!
+        if i == 0 and 0:
+            ## check the first node
+            onOtherBoundary = False
+            for b in nA1.boundSet():
+                if b.marker() > 0 and b.marker() != marker:
+                    onOtherBoundary = True
+            
+            if onOtherBoundary == False:
+                # there is no other (or outer) boundary on the first node so we skip them
+                continue
+            
         nA2 = mesh.createNode(nA1.pos(), nA1.marker())
+        pg._r('create', nA2)
         newNodes.append(nA2)
 
-        # nA2 = newNodes[-1]
         if rC is not None:
             b.setRightCell(None)
             rightCells.append(rC)
@@ -682,6 +703,7 @@ def __Mesh_cutBoundary__(self, marker, boundaryMarker=None):
     for i in range(len(newNodes)-1):
         b = mesh.createBoundary([newNodes[i+1].id(), newNodes[i].id()],
                                 marker=boundaryMarker)
+        print(b)
         b.setLeftCell(rightCells[i])
 Mesh.cutBoundary = __Mesh_cutBoundary__
 
