918bddf6deff99b766b750ef7da584c21eed177e
Thomas Günther <halbmy@pygimli.org>
2023-05-15 11:18:28 +0200

    DOC: finish 2D gravity inversion example

--- doc/examples/4_gravimetry_magnetics/plot_inv-gravity-2d.py
+++ doc/examples/4_gravimetry_magnetics/plot_inv-gravity-2d.py
@@ -4,12 +4,10 @@ r"""
 2D gravity synthetic modelling and inversion
 ============================================
 
-Based on the GIMLi issue #532.
-
 In the following, we will build the model, create synthetic data, and do
 inversion using a depth-weighting function as outlined in the paper.
-
 """
+
 # %%%
 import numpy as np
 import pygimli as pg
@@ -20,37 +18,47 @@ from pygimli.physics.gravimetry import GravityModelling2D
 # %%
 # Synthetic model and data generation
 # -----------------------------------
-# We create a rectangular modelling domain (50x20m) with a flat anomaly  
+# We create a rectangular modelling domain (50x15m) with a flat anomaly  
 # in a depth of about 5m.
 #
 
-x = np.arange(-25, 25.1, .5)
-pnts = np.array([x, np.zeros(len(x))]).T
-
-world = mt.createWorld(start=[-25, 0], end=[25, -20],
+world = mt.createWorld(start=[-25, 0], end=[25, -15],
                        marker=1)
 rect = mt.createRectangle(start=[-6, -3.5], end=[6, -6.0],
                           marker=2, area=0.1)
+rect.rotate([0, 0, 0.15])
 
 geom = world + rect
 pg.show(geom, markers=True)
 mesh = mt.createMesh(geom, quality=33, area=0.2)
 
 # %%%
+# We assume measuring the gravity on a 50m long profile with dense spacing.
 # We initialize the forward response by passing mesh and measuring points.
 # Additionally, we map a density to the cell markers to build a model vector.
 #
+
+x = np.arange(-25, 25.1, .5)
+pnts = np.array([x, np.zeros(len(x))]).T
 
 fop = GravityModelling2D(mesh=mesh, points=pnts)
 dRho = pg.solver.parseMapToCellArray([[1, 0.0], [2, 300]], mesh)
 g = fop.response(dRho)
 
 # %%%
+# We define an absolute error and add some Gaussian noise.
+#
+
+error = 0.0005
+data = g + np.random.randn(len(g)) * error
+
+# %%%
 # The model response is then plotted along with the model
 #
 
 fig, ax = pg.plt.subplots(ncols=1, nrows=2, sharex=True)
-ax[0].plot(x, g)
+ax[0].plot(x, data, "+", label="data")
+ax[0].plot(x, g, "-", label="noisefree")
 ax[0].set_ylabel(r'$\frac{\partial u}{\partial z}$ [mGal]')
 ax[0].grid()
 ax[0].legend()
@@ -61,14 +69,6 @@ ax[1].set_xlabel('$x$-coordinate [m]')
 ax[1].set_ylabel('$z$-coordinate [m]')
 ax[1].set_ylim((-9, 1))
 ax[1].set_xlim((-25, 25))
-
-
-# %%%
-# We define an absolute error and add some Gaussian noise.
-#
-
-error = 0.001
-g += np.random.randn(len(g)) * error
 
 
 # %%%
@@ -92,45 +92,49 @@ fop = GravityModelling2D(mesh=mesh, points=pnts)
 #    w_z = \frac{1}{(z+z_0)^{3/2}}
 #
 
-# depth weighting
-bz = np.array([b.center().z() for b in mesh.boundaries() if not b.outside()])
+cz = -pg.y(mesh.cellCenters())
 z0 = 5
-wz = 10 / (bz+z0)**1.5
+wz = 1 / (cz+z0)**1.5
+pg.plt.plot(cz, wz, ".")
 
 # %%%
 # Inversion
 # ---------
 #
-# The inversion is rather straightforward using the standard inversion
-# framework :py:class:`pygimli.Inversion`.
+# For inversion, we use geostatistic regularization with a higher correlation
+# length for x, compared to y, to account for the large equivalence.
+# We limit the model to reasonable density contrasts of +/- 1000 kg/m^3.
+# As the depth weighting decreases the local regularization weights, we have
+# to increase the overall regularization strength lambda.
 #
 
+fop.region(1).setConstraintType(2)
 inv = pg.Inversion(fop=fop)
-inv.setRegularization(limits=[-2000, 2000], trans="Cot",
-                      correlationLengths=[10, 2])
-inv.setConstraintWeight(wz)
-rho = inv.run(g, absoluteError=error, lam=1e4, verbose=True)
+inv.setRegularization(limits=[-1000, 1000], trans="Cot",
+                      correlationLengths=[12, 2])
+inv.setConstraintWeights(wz)
+rho = inv.run(g, absoluteError=error, lam=1e5, verbose=True)
 
 # %%%
 # Visualization
 # -------------
 #
-# For showing the model, we again use the threshold filter with a value of
-# 0.02. For comparison with the synthetic model, we plot the latter as a
-# wireframe.
+# For showing the model, we again plot model response and model.
 #
 
 fig, ax = pg.plt.subplots(ncols=1, nrows=2, sharex=True)
-ax[0].plot(x, g)
+ax[0].plot(x, data, "+")
+ax[0].plot(x, inv.response, "-")
 ax[0].set_ylabel(r'$\frac{\partial u}{\partial z}$ [mGal]')
 ax[0].grid()
 ax[0].legend()
 
 pg.show(mesh, rho, ax=ax[1], logScale=False)
+pg.viewer.mpl.drawPLC(ax[1], rect, fillRegion=False)
 ax[1].plot(x, x*0, 'bv')
 ax[1].set_xlabel('$x$-coordinate [m]')
 ax[1].set_ylabel('$z$-coordinate [m]')
-ax[1].set_ylim((-9, 1))
+ax[1].set_ylim((-12, 1))
 ax[1].set_xlim((-25, 25))
 
 
