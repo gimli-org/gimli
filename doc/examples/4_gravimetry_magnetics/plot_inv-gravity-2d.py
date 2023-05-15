ec668893d10c11d485119742b735dbdc4a37b6e6
Thomas Günther <halbmy@pygimli.org>
2023-05-15 11:14:45 +0200

    DOC: add 2D gravity inversion example based on #532

--- doc/examples/4_gravimetry_magnetics/plot_inv-gravity-2d.py
+++ doc/examples/4_gravimetry_magnetics/plot_inv-gravity-2d.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 r"""
-3D magnetics modelling and inversion
-====================================
+2D gravity synthetic modelling and inversion
+============================================
 
-Based on the synthetic model of Li & Oldenburg (1996), we demonstrate 3D
-inversion of magnetic data. The forward operator bases on the formula given by
-Holstein et al. (2007).
+Based on the GIMLi issue #532.
 
 In the following, we will build the model, create synthetic data, and do
 inversion using a depth-weighting function as outlined in the paper.
@@ -15,87 +13,71 @@ inversion using a depth-weighting function as outlined in the paper.
 # %%%
 import numpy as np
 import pygimli as pg
-from pygimli.viewer import pv
-from pygimli.physics.gravimetry import MagneticsModelling
+import pygimli.meshtools as mt
+# from pygimli.viewer import pv
+from pygimli.physics.gravimetry import GravityModelling2D
 
 # %%
 # Synthetic model and data generation
 # -----------------------------------
-# The grid is 1x1x0.5 km with a spacing of 50 m.
+# We create a rectangular modelling domain (50x20m) with a flat anomaly  
+# in a depth of about 5m.
 #
 
-dx = 50
-x = np.arange(0., 1001, dx)
-y = np.arange(0., 1001, dx)
-z = np.arange(0., 501, dx)
-grid = pg.createGrid(x, y, z)
-print(grid)
+x = np.arange(-25, 25.1, .5)
+pnts = np.array([x, np.zeros(len(x))]).T
+
+world = mt.createWorld(start=[-25, 0], end=[25, -20],
+                       marker=1)
+rect = mt.createRectangle(start=[-6, -3.5], end=[6, -6.0],
+                          marker=2, area=0.1)
+
+geom = world + rect
+pg.show(geom, markers=True)
+mesh = mt.createMesh(geom, quality=33, area=0.2)
 
 # %%%
-# We create a 3D matrix that is later filled as vector into the grid. The
-# model consists of zeros and patches of 5x6 cells per depth slice that
-# are shifted by one cell for subsequent cells.
+# We initialize the forward response by passing mesh and measuring points.
+# Additionally, we map a density to the cell markers to build a model vector.
 #
 
-v = np.zeros((len(z)-1, len(y)-1, len(x)-1))
-for i in range(7):
-    v[1+i, 11-i:16-i, 7:13] = 0.05
-
-grid["synth"] = v.ravel()
+fop = GravityModelling2D(mesh=mesh, points=pnts)
+dRho = pg.solver.parseMapToCellArray([[1, 0.0], [2, 300]], mesh)
+g = fop.response(dRho)
 
 # %%%
-# We show the model making use of the pyvista package that can be called by
-# :py:func:`pygimli.show`. The mesh itself is shown as a wireframe, the anomaly
-# is plotted as a surface plot using a threshold filter. After the first call
-# with ``hold=True``, the plotter is used to draw any subsequent plots that can
-# also be slices or clips. Moreover, the camera position is set so that the
-# vertical axis is going downwards (x is Northing and y is Easting as common in
-# magnetics).
+# The model response is then plotted along with the model
 #
 
-pl, _ = pg.show(grid, style="wireframe", hold=True)
-pv.drawMesh(pl, grid, label="synth", style="surface", cMap="Spectral_r",
-            filter={"threshold": dict(value=0.05, scalars="synth")})
-pl.camera_position = "yz"
-pl.camera.roll = 90
-pl.camera.azimuth = 180 - 15
-pl.camera.elevation = 10
-pl.camera.zoom(1.2)
-_ = pl.show()
+fig, ax = pg.plt.subplots(ncols=1, nrows=2, sharex=True)
+ax[0].plot(x, g)
+ax[0].set_ylabel(r'$\frac{\partial u}{\partial z}$ [mGal]')
+ax[0].grid()
+ax[0].legend()
+
+pg.show(mesh, dRho, ax=ax[1])
+ax[1].plot(x, x*0, 'bv')
+ax[1].set_xlabel('$x$-coordinate [m]')
+ax[1].set_ylabel('$z$-coordinate [m]')
+ax[1].set_ylim((-9, 1))
+ax[1].set_xlim((-25, 25))
+
 
 # %%%
-# For the computation of the total field, we define the global magnetic
-# field using the IGRF (total field, inclination and declination) settings
-# given in the paper. Any global field can also be retrieved by the
-# ``pyIGRF`` module.
+# We define an absolute error and add some Gaussian noise.
 #
 
-F, I, D = 50000e-9, 75, 25
-H = F * np.cos(np.deg2rad(I))
-X = H * np.cos(np.deg2rad(D))
-Y = H * np.sin(np.deg2rad(D))
-Z = F * np.sin(np.deg2rad(I))
-igrf = [D, I, H, X, Y, Z, F]
+error = 0.001
+g += np.random.randn(len(g)) * error
 
-py, px = np.meshgrid(x, y)
-px = px.ravel()
-py = py.ravel()
-points = np.column_stack((px, py, -np.ones_like(px)*20))
-
-# The forward operator
-cmp = ["TFA"]  # ["Bx", "By", "Bz"]
-fop = MagneticsModelling(grid, points, cmp, igrf)
-model = pg.Vector(grid.cellCount(), 1.0)
-data = fop.response(grid["synth"])
 
 # %%%
-# Just like in the paper, the data are contaminated with an error model
-# consisting of relative and absolute noise of 2% and 1 nT, respectively.
+# For inversion, we create a new mesh from the rectangular domain and setup a
+# new instance of the modelling operator.
 #
 
-err = 0.01
-noise_level = 1e-9
-data += np.random.randn(len(data)) * noise_level
+mesh = mt.createMesh(world, quality=33, area=1)
+fop = GravityModelling2D(mesh=mesh, points=pnts)
 
 # %%%
 # Depth weighting
@@ -111,8 +93,8 @@ data += np.random.randn(len(data)) * noise_level
 #
 
 # depth weighting
-bz = np.array([b.center().z() for b in grid.boundaries() if not b.outside()])
-z0 = 25
+bz = np.array([b.center().z() for b in mesh.boundaries() if not b.outside()])
+z0 = 5
 wz = 10 / (bz+z0)**1.5
 
 # %%%
@@ -123,11 +105,11 @@ wz = 10 / (bz+z0)**1.5
 # framework :py:class:`pygimli.Inversion`.
 #
 
-inv = pg.Inversion(fop=fop, verbose=True)  # , debug=True)
-inv.setRegularization(limits=[0, 0.07])  # to limit values
-inv.setConstraintWeights(wz)
-invmodel = inv.run(data, absoluteError=noise_level, lam=1e4, startModel=1e-3, verbose=True)
-grid["inv"] = invmodel
+inv = pg.Inversion(fop=fop)
+inv.setRegularization(limits=[-2000, 2000], trans="Cot",
+                      correlationLengths=[10, 2])
+inv.setConstraintWeight(wz)
+rho = inv.run(g, absoluteError=error, lam=1e4, verbose=True)
 
 # %%%
 # Visualization
@@ -138,38 +120,19 @@ grid["inv"] = invmodel
 # wireframe.
 #
 
-pl, _ = pg.show(grid, label="synth", style="wireframe", hold=True,
-                filter={"threshold": dict(value=0.025, scalars="synth")})
-pv.drawMesh(pl, grid, label="inv", style="surface", cMap="Spectral_r",
-            filter={"threshold": dict(value=0.02, scalars="inv")})
-pv.drawMesh(pl, grid, label="inv", style="surface", cMap="Spectral_r",
-            filter={"slice": dict(normal=[-1, 0, 0], origin=[500, 600, 250])})
-pl.camera_position = "yz"
-pl.camera.roll = 90
-pl.camera.azimuth = 180 - 15
-pl.camera.elevation = 10
-pl.camera.zoom(1.2)
-_ = pl.show()
+fig, ax = pg.plt.subplots(ncols=1, nrows=2, sharex=True)
+ax[0].plot(x, g)
+ax[0].set_ylabel(r'$\frac{\partial u}{\partial z}$ [mGal]')
+ax[0].grid()
+ax[0].legend()
 
-# %%%
-# The model can nicely outline the top part of the anomalous body, but not
-# its depth extent.
-#
-# We compare the data and model response by means of scatter plots:
-#
+pg.show(mesh, rho, ax=ax[1], logScale=False)
+ax[1].plot(x, x*0, 'bv')
+ax[1].set_xlabel('$x$-coordinate [m]')
+ax[1].set_ylabel('$z$-coordinate [m]')
+ax[1].set_ylim((-9, 1))
+ax[1].set_xlim((-25, 25))
 
-fig, ax = pg.plt.subplots(ncols=2, figsize=(12, 5), sharex=True, sharey=True)
-vals = data * 1e9
-mm = np.max(np.abs(vals))
-ax[0].scatter(px, py, c=vals, cmap="bwr", vmin=-mm, vmax=mm);
-_ = ax[1].scatter(px, py, c=inv.response*1e9, cmap="bwr", vmin=-mm, vmax=mm);
-
-# %%%
-# Alternatively, we can also plot the error-weighted misfit.
-#
-
-misfit = (inv.response - data) / noise_level
-_ = pg.plt.scatter(py, px, c=misfit, cmap="bwr", vmin=-3, vmax=3);
 
 # %%%
 # References
