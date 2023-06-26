faaf451b34f55be73412845cb8512228aa29bf23
Thomas Günther <halbmy@pygimli.org>
2023-06-26 10:27:50 +0200

    ENH: rename ERTIP.showResult into showResults and add LU transform

--- pygimli/physics/ert/ertIPManager.py
+++ pygimli/physics/ert/ertIPManager.py
@@ -26,14 +26,20 @@ class ERTIPManager(ERTManager):
 
     def invertTDIP(self, ipdata=None, **kwargs):
         """IP inversion in time domain."""
-        ipdata = ipdata or self.data["ip"] / 1000
+        ipdata = ipdata or self.data["ip"]
+        if max(ipdata) > 1:  # mV/V
+            ipdata /= 1000
         mesh0 = pg.Mesh(self.paraDomain)
         mesh0.setCellMarkers(mesh0.cellCount())
         fopIP = DCIPMModelling(self.fop, mesh0, self.model, response=self.inv.response)
         fopIP.createRefinedForwardMesh(True)
         invIP = pg.Inversion(fop=fopIP, verbose=True)
+        invIP.modelTrans = pg.trans.TransLogLU(0.0, 1.0)
         errorIP = pg.Vector(self.data.size(), 0.03) + 0.001 / ipdata  # absolute ma 1mV/V plus 3%
-        self.modelIP = invIP.run(ipdata, errorIP, startModel=0.1, lam=10, verbose=True)
+        kwargs.setdefault("lam", 100)
+        kwargs.setdefault("startModel", pg.median(ipdata))
+        kwargs.setdefault("verbose", True)
+        self.modelIP = invIP.run(ipdata, errorIP, **kwargs)
 
     def invertFDIP(self, **kwargs):
         """IP inversion in frequency domain."""
@@ -51,15 +57,26 @@ class ERTIPManager(ERTManager):
 
         self.showModel(self.modelIP*1000, **kwargs)
 
-    def showResult(self, *args, ipkw={}, **kwargs):
-        """Show DC and IP results."""
-        _, ax = pg.plt.subplotsn(rows=2, sharex=True)
-        kwargs.setdefault("ax", ax[0])
-        super().showResult(*args, **kwargs)
+    def showResults(self, reskw={}, ipkw={}, **kwargs):
+        """Show DC and IP results.
+        
+        Parameters
+        ----------
+        reskw : dict
+            keyword arguments for showing resistivity image
+        ipkw : dict
+            keyword arguments for showing IP image
+        **kwargs : dict
+            keyword arguments for showing resistivity image
+        """
+        _, ax = pg.plt.subplots(nrows=2, sharex=True)
+        kwargs.setdefault("orientation", "vertical")
+        reskw.setdefault("ax", ax[0])
+        super().showResult(**reskw, **kwargs)
         ipkw.setdefault("ax", ax[1])
         ipkw.setdefault("logScale", False)
         ipkw.setdefault("cMin", 0)
-        self.showIPModel(ipkw)
+        self.showIPModel(**ipkw, **kwargs)
 
     def invert(self, *args, **kwargs):
         """Carry out DC and IP inversion."""
