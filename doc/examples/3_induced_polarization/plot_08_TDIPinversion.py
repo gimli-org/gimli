485fd826813eea563e62ef94383294556975a9f2
Thomas Günther <halbmy@pygimli.org>
2023-06-29 22:57:20 +0200

    DOC: try fix jenkins gallery

--- doc/examples/3_induced_polarization/plot_08_TDIPinversion.py
+++ doc/examples/3_induced_polarization/plot_08_TDIPinversion.py
@@ -1,4 +1,6 @@
-"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
 Synthetic TDIP modelling and inversion
 ======================================
 This example demonstrates the ERTIPManager introduced in
