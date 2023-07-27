39a22b05ca803a2a1dae684ca92082ec8dd38fc0
carsten-forty2 <carsten@pygimli.org>
2023-07-27 21:27:00 +0200

    FIX. use label keyword for markers=True view or default

--- pygimli/viewer/showmesh.py
+++ pygimli/viewer/showmesh.py
@@ -303,7 +303,8 @@ def showMesh(mesh, data=None, block=False, colorBar=None,
         if mesh.cellCount() > 0:
             uniquemarkers, uniqueidx = np.unique(
                 np.array(mesh.cellMarkers()), return_inverse=True)
-            label = "Cell markers"
+            
+            label = label or "Cell markers"
             cMap = pg.plt.cm.get_cmap("Set3", len(uniquemarkers))
             kwargs["logScale"] = False
             kwargs["cMin"] = -0.5
