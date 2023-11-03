4f4e01b1bd7eea7aff0850db71ef6f4a2a26682e
Thomas Günther <halbmy@pygimli.org>
2023-11-03 23:26:26 +0100

    DOC: fix hydraulic conductivity map and document 3D Darcy flow example

--- doc/examples/5_flow_transport/plot_3D_Darcy_flow.py
+++ doc/examples/5_flow_transport/plot_3D_Darcy_flow.py
@@ -17,16 +17,25 @@ gradient field of :math:`\mathbf{v}=-\nabla p`.
 # sphinx_gallery_thumbnail_number = 2
 
 import numpy as np
-
 import pygimli as pg
 import pygimli.meshtools as mt
 from pygimli.viewer.pv import drawStreamLines, drawSlice
+
+# %%%
+# We start with creating the geometry: a small cube inside of a bigger one,
+# and create a mesh from it.
+#
 
 plc = mt.createCube(size=[40, 20, 15], marker=1, boundaryMarker=0)
 cube = mt.createCube(size=[15, 15, 8], marker=2, boundaryMarker=0)
 geom = plc + cube
 
 mesh = mt.createMesh(geom, area=4)
+
+# %%%
+# For the boundary conditions, we set the markers of all left boundaries to 1
+# and the markers of all righth boundaries to 2.
+#
 
 for bound in mesh.boundaries():
     x = bound.center().x()
@@ -35,17 +44,35 @@ for bound in mesh.boundaries():
     elif x == mesh.xmax():
         bound.setMarker(2)
 
-kMap = {1: 1e-4, 2: 1e-6}
-kArray = pg.solver.parseMapToCellArray(list(kMap), mesh) # dict does not work
-kArray = np.column_stack([kArray] * 3)
+# %%%
+# Next, we create a map for the hydraulic conductivity.
+# The outer cube has 1e-4m/s and the inner 1e-6m/s.
+#
+
+kMap =[[1, 1e-4], [2, 1e-6]]
+kArray = pg.solver.parseMapToCellArray(kMap, mesh)
+
+# %%%
+# We set two different heads for the left and right bounday
+# and solve the Darcy equation using Finite Elements
+#
 
 bc = {"Dirichlet": {1: 20.0, 2: 10.0}}
-
-
 h = pg.solver.solveFiniteElements(mesh, kMap, bc=bc)
-vel = -pg.solver.grad(mesh, h) * kArray
 
+# %%%
+# From the hydraulic head we compute the velocity by
+# multiplying it with the hydraulic conductivity that is
+# before transformed into an isotropic tensor.
+#
+
+kTensor = np.column_stack([kArray] * 3)
+vel = -pg.solver.grad(mesh, h) * kTensor
 pg.show(mesh, h, label="Hydraulic head (m)")
+
+# %%%
+# Last, we show the flow lines that avoid the poor conductor.
+#
 
 ax, _ = pg.show(mesh, alpha=0.3, hold=True, colorBar=False)
 drawStreamLines(ax, mesh, vel, radius=.1, source_radius=10)
