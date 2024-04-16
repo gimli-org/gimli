7631d509a8bf2cfa03a14f76f4c1bd3a41147c98
carsten-forty2 <carsten@pygimli.org>
2024-04-16 16:24:42 +0200

    add jenkin target for newfea branch

---
+++ core/scripts/jenkins-nf.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+# This script is for continuous integration using Jenkins (http://jenkins-ci.org/)
+# It is called for the newfea branch from the jenkins workspace, i.e., source gimli/core/scripts/jenkins-fn.sh
+#rm -rf /var/lib/jenkins/.cache/pygimli # We need a better cleaning process
+
+echo "Starting automatic build #$BUILD_NUMBER on" `date`
+start=$(date +"%s")
+
+# Show last change to repo in build log
+echo `git --git-dir trunk/.git log -1 --pretty="Last change by %cn (%h): %B"`
+
+# Show system information
+lsb_release -d
+uname -a
+gcc --version
+cmake --version
+python --version
+python -c "import numpy; print(numpy.__version__)"
+python -c "import matplotlib; print(matplotlib.__version__)"
+
+# Check if core was changed
+core_update=$(git --git-dir=trunk/.git diff --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep -c core/src || true)
+
+# Set this to 1 if you want clean build (also of dependencies)
+export CLEAN=0
+export GIMLI_NUM_THREADS=$((`nproc --all` - 4))
+
+################
+#  Main build  #
+################
+
+mkdir -p build
+
+pushd build
+    if [ ! -f CMakeCache.txt ]; then
+        # Always rebuild core when Cmake cache does not exist
+        core_update=2
+    fi
+
+    if [[ $core_update -ge 1 ]]; then
+        echo "# Core changes detected. #"
+        touch CMakeCache.txt
+
+        #cmake ../trunk -DPYTHON_EXECUTABLE=/usr/bin/python3
+
+        #make -j $GIMLI_NUM_THREADS
+        #make pygimli J=$GIMLI_NUM_THREADS
+        
+    else
+        echo "# No core changes detected. #"
+    fi
+popd
+
+
+#############################
+#  Testing & documentation  #
+#############################
+
+# # Setup for 3D visualizations
+# # ------------------------------------------------
+# export DISPLAY=:99.0
+# export PYVISTA_OFF_SCREEN=true
+# # ------------------------------------------------
+
+# # Test pygimli
+# export PYTHONPATH=`pwd`/../trunk:$PYTHONPATH
+# OMP_THREAD_LIMIT=4 python -c "import pygimli; print(pygimli.Report()); pygimli.test(show=False, abort=True, htmlreport=\"build_tests.html\", devTests=True)"
+
+# # Build documentation
+# make clean-gallery
+# make doc # = doxygen, sphinxapi, sphinxpdf, sphinxhtml
+# end=$(date +"%s")
+# echo "Ending automatic build #$BUILD_NUMBER".
+# diff=$(($end-$start))
+# echo "$(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
+
+# # If this script fails, a mail with the log file will be send to mail@pygimli.org.
+# # If it succeeds, the documentation will be uploaded to pygimli.org.
+# # In any case, a badge icon of the current status and the log file will be uploaded.
