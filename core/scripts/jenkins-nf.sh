57f32bc6674eb78c6bdaaa3cd319aa05f3e91b8b
carsten-forty2 <carsten@pygimli.org>
2024-04-16 16:34:37 +0200

    fixed some paths to the jenkinsscript

--- core/scripts/jenkins-nf.sh
+++ core/scripts/jenkins-nf.sh
@@ -3,12 +3,14 @@
 # This script is for continuous integration using Jenkins (http://jenkins-ci.org/)
 # It is called for the newfea branch from the jenkins workspace, i.e., source gimli/core/scripts/jenkins-fn.sh
 #rm -rf /var/lib/jenkins/.cache/pygimli # We need a better cleaning process
+GIMLISRC=gimli
+GIMLIBLD=build
 
 echo "Starting automatic build #$BUILD_NUMBER on" `date`
 start=$(date +"%s")
 
 # Show last change to repo in build log
-echo `git --git-dir trunk/.git log -1 --pretty="Last change by %cn (%h): %B"`
+echo `git --git-dir $GIMLISRC/.git log -1 --pretty="Last change by %cn (%h): %B"`
 
 # Show system information
 lsb_release -d
@@ -20,7 +22,7 @@ python -c "import numpy; print(numpy.__version__)"
 python -c "import matplotlib; print(matplotlib.__version__)"
 
 # Check if core was changed
-core_update=$(git --git-dir=trunk/.git diff --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep -c core/src || true)
+core_update=$(git --git-dir=$GIMLISRC/.git diff --name-only $GIT_PREVIOUS_COMMIT $GIT_COMMIT | grep -c core/src || true)
 
 # Set this to 1 if you want clean build (also of dependencies)
 export CLEAN=0
@@ -30,9 +32,9 @@ export GIMLI_NUM_THREADS=$((`nproc --all` - 4))
 #  Main build  #
 ################
 
-mkdir -p build
+mkdir -p $GIMLIBLD
 
-pushd build
+pushd $GIMLIBLD
     if [ ! -f CMakeCache.txt ]; then
         # Always rebuild core when Cmake cache does not exist
         core_update=2
@@ -42,7 +44,7 @@ pushd build
         echo "# Core changes detected. #"
         touch CMakeCache.txt
 
-        #cmake ../trunk -DPYTHON_EXECUTABLE=/usr/bin/python3
+        #cmake ../$GIMLISRC
 
         #make -j $GIMLI_NUM_THREADS
         #make pygimli J=$GIMLI_NUM_THREADS
