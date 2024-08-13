5c2979e8a66019a56880004a9acf1eb74349800d
Thomas Günther <halbmy@pygimli.org>
2024-08-13 08:36:50 +0200

    DOC: simplify additional constraints example a bit

--- doc/examples/6_inversion/plot_2_additionalConstraintsVES.py
+++ doc/examples/6_inversion/plot_2_additionalConstraintsVES.py
@@ -32,15 +32,17 @@ from pygimli.viewer.mpl import drawModel1D
 nlay = 4  # number of layers
 lam = 200.  # (initial) regularization parameter
 errPerc = 3.  # relative error of 3 percent
-ab2 = np.logspace(-1, 2, 50)  # AB/2 distance (current electrodes)
+ab2 = np.logspace(-0.5, 2.5, 50)  # AB/2 distance (current electrodes)
 mn2 = ab2 / 3.  # MN/2 distance (potential electrodes)
 f = VESModelling(ab2=ab2, mn2=mn2, nLayers=nlay)
 synres = [100., 500., 20., 800.]  # synthetic resistivity
 synthk = [0.5, 3.5, 6.]  # synthetic thickness (nlay-th layer is infinite)
 rhoa = f(synthk+synres)
 rhoa = rhoa * (pg.randn(len(rhoa)) * errPerc / 100. + 1.)
-plt.loglog(rhoa, ab2, "x-")
-plt.grid(True)
+fig, ax = plt.subplots()
+ax.loglog(rhoa, ab2, "x-")
+ax.invert_yaxis()
+ax.grid(True)
 
 # %%%
 # Next, we set up an inversion instance with log transformation on data and
@@ -48,16 +50,11 @@ plt.grid(True)
 #
 
 inv = LSQRInversion(fop=f, verbose=True)
-tLog = pg.trans.TransLog()  #
-inv.dataTrans = tLog
-inv.modelTrans = tLog
-startModel = pg.cat(pg.Vector(nlay-1, 8), pg.Vector(nlay, pg.median(rhoa)))
+inv.dataTrans = 'log'
+inv.modelTrans = 'log'
+startModel = [7.0] * (nlay-1) + [pg.median(rhoa)] * nlay
 inv.inv.setMarquardtScheme()
-error = pg.Vector(len(rhoa), errPerc/100)
-model1 = inv.run(rhoa, error, lam=1000, startModel=startModel)
-print(model1)
-print(inv.chi2(), inv.relrms(), pg.sum(inv.model[:nlay-1]))
-
+model1 = inv.run(rhoa, errPerc/100, lam=lam, startModel=startModel)
 
 # %%%
 # To formulate the constraints, we need to set up a matrix for the left side
@@ -76,9 +73,7 @@ for i in range(3):
 
 c = pg.Vector(1, pg.sum(synthk))
 inv.setParameterConstraints(G, c, 100)
-model2 = inv.run(rhoa, error, lam=1000, startModel=startModel)
-print(model2)
-print(inv.chi2(), inv.relrms(), pg.sum(inv.model[:nlay-1]))
+model2 = inv.run(rhoa, errPerc/100, lam=lam, startModel=startModel)
 
 # %%%
 # All three models are plotted together and are equivalently fitting the data.
